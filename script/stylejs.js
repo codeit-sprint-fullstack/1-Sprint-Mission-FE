8befd92596c01d2ecfd25fb8d71ebb73d2ad52cc
woohyuntak <owootak@naver.com>
2024-06-27 18:05:49 +0900

    로그인 완성 CSS and script add

---
+++ script/stylejs.js
@@ -0,0 +1,16 @@
+//아이콘 비밀번호 표시
+const showHiddenPass = (loginpass, passEye) => {
+    const input = document.getElementById(loginpass);
+    const eye = document.getElementById(passEye);
+
+    eye.addEventListener('click', () => {
+        if (input.type === 'password') {
+            input.type = 'text';
+        } else {
+            input.type = 'password';
+        }
+    })
+}
+
+showHiddenPass('password', 'eye_icon');
+
