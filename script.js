3e1f8c7fe58836830126aff0252aaa77062c1e05
jongmin <charlieko123@github.com>
2024-06-28 14:33:49 +0900

    make fuction eyeClick

--- script.js
+++ script.js
@@ -1,55 +1,38 @@
+function clickEye(passwordInput, eye, slashedEye) {
+  eye.addEventListener("click", function() {
+    if(passwordInput.type === "password") {
+      passwordInput.type = "text";
+      eye.style.display = "none"
+      slashedEye.style.display = "block"
+    } else {
+      passwordInput.type = "password";
+      eye.style.display = "block"
+      slashedEye.style.display = "none"
+    }
+  });
+  slashedEye.addEventListener("click", function() {
+    if(passwordInput.type === "password") {
+      passwordInput.type = "text";
+      eye.style.display = "none"
+      slashedEye.style.display = "block"
+    } else {
+      passwordInput.type = "password";
+      eye.style.display = "block"
+      slashedEye.style.display = "none"
+    }
+  });
+}
+
 const passwordBox = document.querySelector(".password.inputBox");
 const passwordInput = document.querySelector(".password.inputForm");
 const eye = passwordBox.querySelector(".eye");
 const slashedEye = passwordBox.querySelector(".slashed.eye");
 
-eye.addEventListener("click", function() {
-  if(passwordInput.type === "password") {
-    passwordInput.type = "text";
-    eye.style.display = "none"
-    slashedEye.style.display = "block"
-  } else {
-    passwordInput.type = "password";
-    eye.style.display = "block"
-    slashedEye.style.display = "none"
-  }
-});
-slashedEye.addEventListener("click", function() {
-  if(passwordInput.type === "password") {
-    passwordInput.type = "text";
-    eye.style.display = "none"
-    slashedEye.style.display = "block"
-  } else {
-    passwordInput.type = "password";
-    eye.style.display = "block"
-    slashedEye.style.display = "none"
-  }
-});
+clickEye(passwordInput, eye, slashedEye);
 
 const passwordBox2 = document.querySelector(".check.password.inputBox");
 const passwordInput2 = document.querySelector(".check.password.inputForm");
 const eye2 = passwordBox2.querySelector(".eye");
 const slashedEye2 = passwordBox2.querySelector(".slashed.eye");
 
-eye2.addEventListener("click", function() {
-  if(passwordInput2.type === "password") {
-    passwordInput2.type = "text";
-    eye2.style.display = "none"
-    slashedEye2.style.display = "block"
-  } else {
-    passwordInput2.type = "password";
-    eye2.style.display = "block"
-    slashedEye2.style.display = "none"
-  }
-});
-slashedEye2.addEventListener("click", function() {
-  if(passwordInput2.type === "password") {
-    passwordInput2.type = "text";
-    eye2.style.display = "none"
-    slashedEye2.style.display = "block"
-  } else {
-    passwordInput2.type = "password";
-    eye2.style.display = "block"
-    slashedEye2.style.display = "none"
-  }
-});
+clickEye(passwordInput2, eye2, slashedEye2);
