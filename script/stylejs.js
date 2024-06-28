0171fa41674c9661f68eac34c4581a06e2b26a19
woohyuntak <owootak@naver.com>
2024-06-28 11:59:59 +0900

    style and js modi

--- script/stylejs.js
+++ script/stylejs.js
@@ -15,13 +15,30 @@ const showHiddenPass = (loginpass, passEye) => {
 showHiddenPass('password_first', 'eye_icon');
 showHiddenPass('password_conf_label', 'conf_eye_icon');
 
-const password_first = document.getElementById(password_first);
-const confirmPassword = document.getElementById(password_conf_label);
-const confirm_item = document.getElementById(confirm_item);
+const password_first = document.getElementById('password_first');
+const confirmPassword = document.getElementById('password_conf_label');
+const confirm_item = document.getElementById('confirm_pass_item');
+const password_item = document.getElementById('password_item');
 
-confirmPassword.onclick = function () {
-    confirm_item.style.color = 'blue';
-    if (password_first.value !== confirmPassword.value) {
-        confirm_item.style.color = 'blue';
+const passwordRegex = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,15}$/;
+
+password_first.onkeyup = function () {
+    if (password_item.value != 0) {
+        if (passwordRegex.test(password_first.value)) {
+            password_item.classList.add('hide');
+        } else {
+            password_item.classList.remove('hide');
+        }
+    } else {
+        password_item.classList.add('hide');
+    }
+}
+
+confirmPassword.onkeyup = function () {
+    if (password_first.value === confirmPassword.value) {
+        confirm_item.classList.add('hide');
+    }
+    else {
+        confirm_item.classList.remove('hide');
     }
 }
