e2a0b7525fb6a61ac532661c3b77966546680142
jongmin <charlieko123@github.com>
2024-07-06 23:16:43 +0900

    add nickname valid

--- validCheck.js
+++ validCheck.js
@@ -2,6 +2,17 @@ const email = document.getElementById('email');
 const emailError = document.querySelector('.email.errorMessage');
 const emailReg = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
 
+const password = document.getElementById('password');
+const passwordError = document.querySelector('.password.errorMessage');
+const checkPassword = document.getElementById('checkPassword');
+const checkPasswordError = document.querySelector('.checkPassword.errorMessage');
+const nickName = document.getElementById('name');
+const nickNameError = document.querySelector('.name.errorMessage');
+
+const loginButton = document.querySelector('.loginButton');
+const joinButton = document.querySelector('.joinButton');
+
+/*이메일 검사*/
 email.addEventListener('focusout',() => {
   if(!email.value){
     email.classList.add('error');
@@ -14,12 +25,7 @@ email.addEventListener('focusout',() => {
     emailError.textContent = '';
   }
 });
-
-const password = document.getElementById('password');
-const passwordError = document.querySelector('.password.errorMessage');
-const checkPassword = document.getElementById('checkPassword');
-const checkPasswordError = document.querySelector('.checkPassword.errorMessage');
-
+/*비밀번호 검사*/
 password.addEventListener('focusout', () => {
   if(!password.value){
     password.classList.add('error');
@@ -37,7 +43,7 @@ password.addEventListener('focusout', () => {
     checkPasswordError.textContent = '';
   }
 });
-
+/*비밀번호 확인*/
 checkPassword.addEventListener('focusout', () => {
   if(!checkPassword.value){
     checkPassword.classList.add('error');
@@ -48,5 +54,15 @@ checkPassword.addEventListener('focusout', () => {
   } else {
     checkPassword.classList.remove('error');
     checkPasswordError.textContent = '';
+  }
+});
+/*닉네임 확인*/
+nickName.addEventListener('focusout', () => {
+  if(!nickName.value){
+    nickName.classList.add('error');
+    nickNameError.textContent = '닉네임을 입력해주세요.';
+  } else {
+    nickName.classList.remove('error');
+    nickNameError.textContent = '';
   }
 });
