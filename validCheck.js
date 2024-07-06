55311c1d3191344f6730f99058ebbd7866bcfb42
jongmin <charlieko123@github.com>
2024-07-06 22:13:29 +0900

    add password valid check

--- validCheck.js
+++ validCheck.js
@@ -1,5 +1,5 @@
 const email = document.getElementById('email');
-const emailError = document.querySelector('.email.errorMessage')
+const emailError = document.querySelector('.email.errorMessage');
 const emailReg = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
 
 email.addEventListener('focusout',() => {
@@ -15,4 +15,19 @@ email.addEventListener('focusout',() => {
   }
 });
 
+const password = document.getElementById('password');
+const passwordError = document.querySelector('.password.errorMessage');
+
+password.addEventListener('focusout', () => {
+  if(!password.value){
+    password.classList.add('error');
+    passwordError.textContent = '비밀번호를 입력해주세요.';
+  } else if(String(password.value).length < 8){
+    password.classList.add('error');
+    passwordError.textContent = '비밀번호를 8자 이상 입력해주세요.';
+  } else {
+    password.classList.remove('error');
+    passwordError.textContent = '';
+  }
+});
 
