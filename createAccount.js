b8c8c85647561e023d56db62449c5d18b9bdf34c
sohee <s2h110@naver.com>
2024-07-07 17:40:31 +0900

    Add submit event

--- createAccount.js
+++ createAccount.js
@@ -7,5 +7,24 @@ const USER_DATA = [
     { email: 'codeit5@codeit.com', password: "codeit505!" },
     { email: 'codeit6@codeit.com', password: "codeit606!" },
     ];
+
+/* 데이터베이스에 있는 이메일/비밀번호 인지 확인 */
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    /* 이메일과 비밀번호 입력받음 */
+    const email = document.getElementById('email').value;
+
+    /* 데이터베이스에서 이메일을 이용해서 사용자를 찾는다 */ 
+    let foundUser = USER_DATA.find(function founduser(user){ 
+      return user.email === email; 
+    });
+
+    if (foundUser) { // 이미 데이터베이스에 있는 이메일인 경우
+      alert("사용중인 이메일입니다.");
+    } else {
+      alert("회원가입에 성공했습니다!");
+      window.location.replace('/login');
+    }
+  });
      
-    
