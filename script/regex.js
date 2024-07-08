ad9b5e07e1acf999d786bc850ea6242a366e996a
woohyuntak <owootak@naver.com>
2024-07-08 11:32:17 +0900

    login regex

--- script/regex.js
+++ script/regex.js
@@ -1,17 +1,19 @@
 const pwdRegex = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,15}$/;
 const email_regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i;
 
-export function emailRegex (e) {
+export function emailRegex(e) {
     const err = e.target.parentElement.nextElementSibling;
 
-    if(!e.target.value) {
+    if (!e.target.value) {
         err.textContent = '이메일을 입력해주세요.';
         err.classList.remove('hide');
+        err.disabled = true;
         e.target.style.borderColor = '#F74747';
         e.target.style.borderStyle = 'solid';
-    } else if(!email_regex.test(e.target.value)) {
+    } else if (!email_regex.test(e.target.value)) {
         err.textContent = '잘못된 이메일 형식입니다.';
         err.classList.remove('hide');
+        err.disabled = true;
         e.target.style.borderColor = '#F74747';
         e.target.style.borderStyle = 'solid';
     } else {
@@ -20,16 +22,31 @@ export function emailRegex (e) {
     }
 }
 
-export function passwordRegex (e) {
+export function nameRegex(e) {
     const err = e.target.parentElement.nextElementSibling;
 
-    if(!e.target.value) {
+    if (!e.target.value) {
+        err.textContent = '닉네임을 입력해주세요.';
+        err.classList.remove('hide');
+        err.disabled = true;
+        e.target.style.borderColor = '#F74747';
+        e.target.style.borderStyle = 'solid';
+    } else {
+        err.classList.add('hide');
+        e.target.style.borderStyle = 'none';
+    }
+}
+
+export function passwordRegex(e) {
+    const err = e.target.parentElement.nextElementSibling;
+
+    if (!e.target.value) {
         err.textContent = '비밀번호를 입력해주세요.';
         err.classList.remove('hide');
         e.target.style.borderColor = '#F74747';
         e.target.style.borderStyle = 'solid';
-    } else if(e.target.value.length < 8) {
-        err.textContent = '비밀번호를 8자 이상 입력해주세요.';
+    } else if (!pwdRegex.test(e.target.value)) {
+        err.textContent = '비밀번호는 특수문자 + 영문 + 숫자 입니다.';
         err.classList.remove('hide');
         e.target.style.borderColor = '#F74747';
         e.target.style.borderStyle = 'solid';
@@ -38,3 +55,47 @@ export function passwordRegex (e) {
         e.target.style.borderStyle = 'none';
     }
 }
+
+export function passwordConfirmRegex(e, pwd) {
+    const err = e.target.parentElement.nextElementSibling;
+
+    if (!e.target.value !== pwd.value) {
+        err.textContent = '비밀번호가 같지 않습니다.';
+        err.classList.remove('hide');
+        e.target.style.borderColor = '#F74747';
+        e.target.style.borderStyle = 'solid';
+    } else {
+        err.classList.add('hide');
+        e.target.style.borderStyle = 'none';
+    }
+}
+
+// export function disabledBtn(id, pwd) {
+//     if (email_regex.test(id.value) && pwdRegex.test(pwd.value)) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
+export function disabledBtn(arr) {
+
+    Array.from(arr).forEach((er) => {
+        if (!er.value) {
+            console.log(er.value);
+            return false
+        } else {
+            console.log(er.value);
+            return true;
+        }
+    })
+}
+
+export const USER_DATA = [
+    { email: 'codeit1@codeit.com', password: "codeit101!" },
+    { email: 'codeit2@codeit.com', password: "codeit202!" },
+    { email: 'codeit3@codeit.com', password: "codeit303!" },
+    { email: 'codeit4@codeit.com', password: "codeit404!" },
+    { email: 'codeit5@codeit.com', password: "codeit505!" },
+    { email: 'codeit6@codeit.com', password: "codeit606!" },
+];
