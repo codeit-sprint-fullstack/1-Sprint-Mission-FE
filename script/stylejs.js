b74a314219bd1bd62c02078288f2ccc8185071f3
woohyuntak <owootak@naver.com>
2024-07-08 14:16:20 +0900

    text regx modi

--- script/stylejs.js
+++ script/stylejs.js
@@ -1,4 +1,4 @@
-import { USER_DATA, disabledBtn, emailRegex, nameRegex, passwordRegex, passwordConfirmRegex } from './regex.js';
+import { USER_DATA, emailRegex, nameRegex, passwordRegex, passwordConfirmRegex, inputDisabledBtn } from './regex.js';
 
 //아이콘 비밀번호 표시
 const showHiddenPass = (loginpass, passEye) => {
@@ -13,18 +13,14 @@ const showHiddenPass = (loginpass, passEye) => {
         }
     })
 }
-
 showHiddenPass('password_first', 'eye_icon');
-// showHiddenPass('password_conf_label', 'conf_eye_icon');
+showHiddenPass('password_conf_label', 'conf_eye_icon');
 
 //유효성검사
 const password_first = document.querySelector('#password_first');
-const confirmPassword = document.querySelector('#password_conf_label');
-const confirm_item = document.querySelector('#confirm_pass_item');
 const userId = document.querySelector('#user_id');
-const password_item = document.querySelector('#password_item');
 const inputCheck = document.getElementsByTagName('input');
-const hideCheck = document.getElementsByClassName('hide');
+const hideCheck = document.getElementsByClassName('meg');
 const form = document.querySelector('#form');
 const submit_btn = document.querySelector('#submit_btn');
 
@@ -32,47 +28,22 @@ form.addEventListener('focusout', (e) => {
     switch (e.target.dataset.content) {
         case 'email':
             emailRegex(e);
-            if (disabledBtn(inputCheck)) {
-                console.log('성공');
-                submit_btn.disabled = false;
-                submit_btn.style.backgroundColor = '#3692FF';
-            } else {
-                console.log('실패');
-                submit_btn.disabled = true;
-                submit_btn.style.backgroundColor = '#9CA3AF';
-            }
             break;
         case 'name':
             nameRegex(e);
-            if (disabledBtn(userId, password_first)) {
-                submit_btn.disabled = false;
-                submit_btn.style.backgroundColor = '#3692FF';
-            } else {
-                submit_btn.disabled = true;
-                submit_btn.style.backgroundColor = '#9CA3AF';
-            }
             break;
         case 'pwd':
             passwordRegex(e);
-            if (disabledBtn(inputCheck)) {
-                console.log('성공');
-                submit_btn.disabled = false;
-                submit_btn.style.backgroundColor = '#3692FF';
-            } else {
-                console.log('실패');
-                submit_btn.disabled = true;
-                submit_btn.style.backgroundColor = '#9CA3AF';
-            }
             break;
         case 'pwdconfirm':
             passwordConfirmRegex(e, password_first);
-            if (disabledBtn(userId, password_first)) {
-                submit_btn.disabled = false;
-                submit_btn.style.backgroundColor = '#3692FF';
-            } else {
-                submit_btn.disabled = true;
-                submit_btn.style.backgroundColor = '#9CA3AF';
-            }
             break;
+    }
+    if (inputDisabledBtn(hideCheck, inputCheck)) {
+        submit_btn.disabled = false;
+        submit_btn.style.backgroundColor = '#3692FF';
+    } else {
+        submit_btn.disabled = true;
+        submit_btn.style.backgroundColor = '#9CA3AF';
     }
 })
