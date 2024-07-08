097b2894cc72cb3b703e2ed5f0cfaeea36590f0e
sanho <dieuaimer@gmail.com>
2024-07-08 21:18:53 +0900

    로그인 페이지에서 이메일, 비밀번호 유효성 검사 기능 구현

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const visibilityIcon = document.querySelector(".visibility-icon");
 
 const loginForm = document.querySelector('.login-form');
 const userEmail = document.querySelector(".email-container");
+const userPassword = document.querySelector(".password-container");
 
 
 // Function for password visualization
@@ -23,13 +24,13 @@ const showError = (inputTag, msg) => {
   const isErrorMsg = inputTag.lastElementChild.tagName === 'P';
   // Error 메시지가 존재한다면 메시지 변경
   if (isErrorMsg) {
-    inputTag.children[1].classList.remove('email-input');
+    // inputTag.children[1].classList.remove('email-input');
     inputTag.children[1].classList.add('err-line');
     
     inputTag.lastElementChild.textContent = msg;
   // Error 메시지가 존재하지 않는다면 p tag 생성 후 메시지 추가
   } else {
-    inputTag.children[1].classList.remove('email-input');
+    // inputTag.children[1].classList.remove('email-input');
     inputTag.children[1].classList.add('err-line');
 
     const errorMsg = document.createElement('p');
@@ -39,9 +40,9 @@ const showError = (inputTag, msg) => {
   }
 }
 
-const hidenError = (inputTag) => {
+const hideError = (inputTag) => {
   inputTag.children[1].classList.remove('err-line');
-  inputTag.children[1].classList.add('email-input');
+  inputTag.children[1].classList.add('input');
   inputTag.lastElementChild.textContent = ''; 
 }
 
@@ -52,14 +53,30 @@ userEmail.children[1].addEventListener('blur', (e)=>{
   const emailRegEx = /^[A-Za-z0-9-\.\-]+@[A-Za-z0-9\-]+\.[A-za-z0-9\-]+/;
   const validEmail = emailRegEx.test(emailValue);
 
+  
   if (!emailValue) {
-    showError(userEmail, '이메일을 입력해주세요.');
-  } else if (!validEmail) {
-    showError(userEmail, '잘못된 이메일 형식입니다.');
-  } else {
-    hidenError(userEmail);
+    showError(userEmail, '이메일을 입력해주세요.'); // focus-in 후에 입력된 값이 없는 경우
+  } else if (!validEmail) { 
+    showError(userEmail, '잘못된 이메일 형식입니다.'); // 형식이 잘못된 경우 
+  } else { // 제대로 입력했을 경우
+    hideError(userEmail);
   }
 });
+
+userPassword.children[1].addEventListener('blur', (e)=>{
+  const passwordValue = e.target.value;
+
+  const validPassword = passwordValue.length >= 8;
+  console.log(validPassword);
+
+  if (!passwordValue) {
+    showError(userPassword, '비밀번호를 입력해주세요.');
+  } else if (!validPassword) {
+    showError(userPassword, '비밀번호를 8자 이상 입력해주세요.');
+  } else {
+    hideError(userPassword);
+  }
+})
 
 // Function to prevent submission when press enter key
 loginForm.addEventListener('submit', (e) => {
