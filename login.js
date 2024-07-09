386bf292d824115786ed5b787ec61bcf4468608c
wxy0415 <qkrtkd727@gmail.com>
2024-07-09 14:12:15 +0900

    로그인 유효성검사 모바일 버전 추가됐습니다

--- login.js
+++ login.js
@@ -1,3 +1,7 @@
+// 비밀번호 숨기기/표시하기
+const password = document.querySelector('.password');
+const passIcon = document.querySelector('.pass_icon');
+
 function showHide(b,a) {
    if (b.type === 'password') {
    b.setAttribute('type','text');
@@ -8,24 +12,14 @@ function showHide(b,a) {
  }
 };
 
-const password = document.querySelector('.password');
-const passIcon = document.querySelector('.pass_icon');
-
 passIcon.addEventListener('click', () => {showHide(password,passIcon)})
 
-
-
-
-
-
-
-
+// 이메일 유효성 검사
+const input = document.querySelector('input');
 const email = document.getElementById('user-email');
 const failureMessage = document.querySelector('.failure-message');
 const failureMessageTwo = document.querySelector('.failure-message2');
-const input = document.querySelector('input');
-
-const pattern = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/;
+const pattern = /^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]{2,3}$/;
 
 email.addEventListener('focusout', () => {
   if (email.value === '') {
@@ -38,16 +32,26 @@ email.addEventListener('focusout', () => {
     failureMessageTwo.classList.remove('hide');
     input.classList.add('invalid-value');
     input.classList.remove('valid-value');
-    } else {
-      failureMessage.classList.add('hide');
-      failureMessageTwo.classList.add('hide');
-      input.classList.remove('invalid-value');
-      input.classList.add('valid-value');
     }
   }
-)
+);
 
+// 이메일 포커스
+email.addEventListener('focusin', () => {
+  input.classList.add('blue-border');
+})
 
+email.addEventListener('keyup', () => {
+  if (pattern.test(email.value)) {
+      failureMessage.classList.add('hide');
+      failureMessageTwo.classList.add('hide');
+      email.classList.remove('invalid-value');
+      email.classList.add('valid-value');
+    }
+});
+ 
+
+// 비밀버호 유효성 검사
 const failureMessageThree = document.querySelector('.failure-message3');
 const failureMessageFour = document.querySelector('.failure-message4');
 
@@ -62,12 +66,49 @@ password.addEventListener('focusout', () => {
     failureMessageFour.classList.remove('hide');
     password.classList.add('invalid-value');
     password.classList.remove('valid-value');
-    } else {
+    }
+});
+
+password.addEventListener('keyup', () => {
+  if (password.value.length >= 8) {
       failureMessageThree.classList.add('hide');
       failureMessageFour.classList.add('hide');
       password.classList.remove('invalid-value');
       password.classList.add('valid-value');
-  }
-})
+    } 
+});
  
-  
+// 비밀번호 포커스
+  password.addEventListener('focusin', () => {
+  password.classList.add('blue-border');
+});
+
+// 버튼 활성화
+const loginBtn = document.querySelector('button');
+
+email.addEventListener('keyup', btnActivate);
+password.addEventListener('keyup', btnActivate);
+function btnActivate() {
+  switch((pattern.test(email.value)) && password.value.length >= 8) {
+    case false : 
+    loginBtn.disabled = true; 
+    loginBtn.classList.remove('loginBtn_activate')
+    break;
+    
+    case true :
+    loginBtn.disabled = false;
+    loginBtn.classList.add('loginBtn_activate')
+    break;
+  }
+}
+
+
+// 유저 데이터 베이스
+const USER_DATA = [
+  { email: 'codeit1@codeit.com', password: "codeit101!" },
+  { email: 'codeit2@codeit.com', password: "codeit202!" },
+  { email: 'codeit3@codeit.com', password: "codeit303!" },
+  { email: 'codeit4@codeit.com', password: "codeit404!" },
+  { email: 'codeit5@codeit.com', password: "codeit505!" },
+  { email: 'codeit6@codeit.com', password: "codeit606!" },
+];
