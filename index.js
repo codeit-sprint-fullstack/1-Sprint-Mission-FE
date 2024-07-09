ae4b0e13cea3eeed2666c6289ebc2d0cd7494433
sanho <dieuaimer@gmail.com>
2024-07-09 14:07:53 +0900

    feat: Add function to check if the login button should be enabled or disabled

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const visibilityIcon = document.querySelector(".visibility-icon");
 
 const loginForm = document.querySelector('.login-form');
-const userEmail = document.querySelector(".email-container");
-const userPassword = document.querySelector(".password-container");
+const userEmailContainer = document.querySelector(".email-container");
+const userPasswordContainer = document.querySelector(".password-container");
 
+const loginButton = document.querySelector("#button");
 
 // Function for password visualization
 const passwordVisibility = (e) => {
@@ -15,70 +16,92 @@ const passwordVisibility = (e) => {
   e.target.height = 24;
 }
 
-visibilityIcon.addEventListener('click', passwordVisibility);
+// Functions for validating user login information
+const validateEmail = (email) => {
+  const emailRegEx = /^[A-Za-z0-9-\.\-]+@[A-Za-z0-9\-]+\.[A-za-z0-9\-]+/;
+  return emailRegEx.test(email);
+}
+
+const validatePassword = (password) => {
+  return password.length >= 8;
+}
 
 
-// Function for validation of user's email and password
+// Function to check if the login button should be enabled or disabled
+const checkLoginButtonStatus = () => {
+  const emailValue = userEmailContainer.children[1].value;
+  const passwordValue = userPasswordContainer.children[1].value;
+
+  const emailErrorMsg = userEmailContainer.querySelector(".err-msg");
+  const passwordErrorMsg = userPasswordContainer.querySelector(".err-msg");
+
+  if (emailErrorMsg || passwordErrorMsg) {
+    loginButton.disabled = true;
+    loginButton.classList.remove('enabled')
+  } else if (validateEmail(emailValue) && validatePassword(passwordValue)) {
+    loginButton.disabled = false;
+    loginButton.classList.add('enabled');
+  }
+}
+
+// Functions for validation of user's email and password
 const showError = (inputTag, msg) => {
-  // 입력 tag의 마지막 자식요소가 p 태그라면 error 메시지 존재  
   const isErrorMsg = inputTag.lastElementChild.tagName === 'P';
-  // Error 메시지가 존재한다면 메시지 변경
-  if (isErrorMsg) {
-    // inputTag.children[1].classList.remove('email-input');
-    inputTag.children[1].classList.add('err-line');
-    
-    inputTag.lastElementChild.textContent = msg;
-  // Error 메시지가 존재하지 않는다면 p tag 생성 후 메시지 추가
-  } else {
-    // inputTag.children[1].classList.remove('email-input');
-    inputTag.children[1].classList.add('err-line');
+  inputTag.children[1].classList.remove('input');
+  inputTag.children[1].classList.add('err-line');
 
+  if (isErrorMsg) {
+    inputTag.lastElementChild.textContent = msg;
+  } else {
     const errorMsg = document.createElement('p');
     errorMsg.classList.add('err-msg');
     errorMsg.textContent = msg;
     inputTag.append(errorMsg);
   }
+  checkLoginButtonStatus();
 }
 
 const hideError = (inputTag) => {
   inputTag.children[1].classList.remove('err-line');
   inputTag.children[1].classList.add('input');
-  inputTag.lastElementChild.textContent = ''; 
+  const errorMsg = inputTag.querySelector('.err-msg');
+  if (errorMsg) {
+    errorMsg.remove();
+  }
+  checkLoginButtonStatus();
 }
 
 
-userEmail.children[1].addEventListener('blur', (e)=>{
+const handleEmailValidation = (e) => {
   const emailValue = e.target.value;
-  
-  const emailRegEx = /^[A-Za-z0-9-\.\-]+@[A-Za-z0-9\-]+\.[A-za-z0-9\-]+/;
-  const validEmail = emailRegEx.test(emailValue);
-
-  
   if (!emailValue) {
-    showError(userEmail, '이메일을 입력해주세요.'); // focus-in 후에 입력된 값이 없는 경우
-  } else if (!validEmail) { 
-    showError(userEmail, '잘못된 이메일 형식입니다.'); // 형식이 잘못된 경우 
-  } else { // 제대로 입력했을 경우
-    hideError(userEmail);
-  }
-});
-
-userPassword.children[1].addEventListener('blur', (e)=>{
-  const passwordValue = e.target.value;
-
-  const validPassword = passwordValue.length >= 8;
-  console.log(validPassword);
-
-  if (!passwordValue) {
-    showError(userPassword, '비밀번호를 입력해주세요.');
-  } else if (!validPassword) {
-    showError(userPassword, '비밀번호를 8자 이상 입력해주세요.');
+    showError(userEmailContainer, '이메일을 입력해주세요.');
+  } else if (!validateEmail(emailValue)) {
+    showError(userEmailContainer, '잘못된 이메일 형식입니다.')
   } else {
-    hideError(userPassword);
+    hideError(userEmailContainer);
   }
-})
+}
 
-// Function to prevent submission when press enter key
-loginForm.addEventListener('submit', (e) => {
+const handlePasswordValidation = (e) => {
+  const passwordValue = e.target.value;
+  if (!passwordValue) {
+    showError(userPasswordContainer, '비밀번호를 입력해주세요.');
+  } else if (!validatePassword(passwordValue)) {
+    showError(userPasswordContainer, '비밀번호를 8자 이상 입력해주세요.'); 
+  } else {
+    hideError(userPasswordContainer);
+  }
+}
+
+const handleFormSubmit = (e) => {
   e.preventDefault();
-})
+}
+
+
+loginForm.addEventListener('submit', handleFormSubmit);
+visibilityIcon.addEventListener('click', passwordVisibility);
+userEmailContainer.children[1].addEventListener('blur', handleEmailValidation);
+userPasswordContainer.children[1].addEventListener('blur', handlePasswordValidation);
+userEmailContainer.children[1].addEventListener('change', checkLoginButtonStatus);
+userPasswordContainer.children[1].addEventListener('change', checkLoginButtonStatus);
