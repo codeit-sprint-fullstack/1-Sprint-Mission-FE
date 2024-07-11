7f8816d51493a640b5a18fa544e1a9859c04f754
wxy0415 <qkrtkd727@gmail.com>
2024-07-11 14:19:02 +0900

    회원가입창 완성했습니다

--- login/login.js
+++ login/login.js
@@ -1,25 +1,10 @@
 // 비밀번호 숨기기/표시하기
-const password = document.querySelector('.password');
-const passIcon = document.querySelector('.pass_icon');
-
-function showHide(b,a) {
-  if (b.type === 'password') {
-    b.setAttribute('type','text');
-    a.setAttribute('src', 'img/eyeopen.png');
-  } else {
-    b.setAttribute('type', 'password');
-    a.setAttribute('src', 'img/eyeclose.png');
-  }
-};
-
-passIcon.addEventListener('click', () => {showHide(password,passIcon)})
+import { password, passIcon, showHide } from '../components/showHide.js';
+passIcon.addEventListener('click', () => showHide(password,passIcon));
 
 // 이메일 유효성 검사
-const input = document.querySelector('input');
-const userEmail = document.getElementById('user-email');
-const failureMessage = document.querySelector('.failure-message');
-const failureMessageTwo = document.querySelector('.failure-message2');
-const pattern = /^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]{2,3}$/;
+import { failureMessage, failureMessageTwo, input, userEmail } from '../components/emailValid.js';
+import { pattern } from '../components/regex.js'
 
 userEmail.addEventListener('focusout', () => {
   if (userEmail.value === '') {
@@ -32,10 +17,8 @@ userEmail.addEventListener('focusout', () => {
     failureMessageTwo.classList.remove('hide');
     input.classList.add('invalid-value');
     input.classList.remove('valid-value');
-    }
   }
-);
-
+});
 userEmail.addEventListener('keyup', () => {
   if (pattern.test(userEmail.value)) {
       failureMessage.classList.add('hide');
@@ -45,12 +28,7 @@ userEmail.addEventListener('keyup', () => {
     }
 });
 
-// 이메일 포커스
-userEmail.addEventListener('focusin', () => {
-  input.classList.add('blue-border');
-})
-
-// 비밀버호 유효성 검사
+// 비밀번호 유효성 검사
 const failureMessageThree = document.querySelector('.failure-message3');
 const failureMessageFour = document.querySelector('.failure-message4');
 
@@ -74,19 +52,12 @@ password.addEventListener('keyup', () => {
       failureMessageFour.classList.add('hide');
       password.classList.remove('invalid-value');
       password.classList.add('valid-value');
-    } 
-});
- 
-// 비밀번호 포커스
-  password.addEventListener('focusin', () => {
-  password.classList.add('blue-border');
+    }
 });
 
 // 버튼 활성화
 const loginBtn = document.querySelector('.login_btn');
 
-userEmail.addEventListener('keyup', btnActivate);
-password.addEventListener('keyup', btnActivate);
 function btnActivate() {
   switch((pattern.test(userEmail.value)) && password.value.length >= 8) {
     case false : 
@@ -101,28 +72,30 @@ function btnActivate() {
   }
 }
 
-// 유저 데이터 베이스, 모달
-const USER_DATA = [
-  { email: 'codeit1@codeit.com', password: 'codeit101!' },
-  { email: 'codeit2@codeit.com', password: 'codeit202!' },
-  { email: 'codeit3@codeit.com', password: 'codeit303!' },
-  { email: 'codeit4@codeit.com', password: 'codeit404!' },
-  { email: 'codeit5@codeit.com', password: 'codeit505!' },
-  { email: 'codeit6@codeit.com', password: 'codeit606!' },
-];
+userEmail.addEventListener('keyup', btnActivate);
+password.addEventListener('keyup', btnActivate);
 
+// 유저 데이터 베이스, 모달
 const modalCloseButton = document.getElementById('modalCloseButton');
 const modal = document.getElementById('modalContainer');
+import { USER_DATA } from '../components/userData.js';
 
 loginBtn.addEventListener('click', () => {
-  const user = USER_DATA.find(userData => userData.email === userEmail.value)
-  if(user.password === password.value) {
-    window.location.href = 'item.html'
+  const user = USER_DATA.find(userData => userData.email === userEmail.value.trim())
+  if (!user) {
+    modal.classList.remove('hidden') 
+  } else if (user.password === password.value) {
+    window.location.href = '../item/index.html'
   } else {
-    modal.classList.remove('hidden')  
+    modal.classList.remove('hidden') 
   }
-})
+});
+
 modalCloseButton.addEventListener('click', () => {
   modal.classList.add('hidden')
-})
+});
 
+// 인풋 포커스
+import focusIn from '../components/focus.js';
+userEmail.addEventListener('focusin', () => {focusIn(input)});
+password.addEventListener('focusin', () => {focusIn(password)});
--- signup/signup.js
+++ signup/signup.js
@@ -1,33 +1,19 @@
-// 비밀번호 숨기기
-const password = document.querySelector('.password');
-const passIcon = document.querySelector('.pass_icon');
+// 비밀번호 표시하기/숨기기
 const checkIcon = document.querySelector('.check_icon');
 const passwordCheck = document.querySelector('.password_check');
+import { password, passIcon, showHide } from '../components/showHide.js '
 
-function showHide(b,a) {
-    if (b.type === 'password') {
-    b.setAttribute('type','text');
-    a.setAttribute('src', 'img/eyeopen.png');
-    } else {
-    b.setAttribute('type', 'password');
-    a.setAttribute('src', 'img/eyeclose.png');
-  }
-};
-
-passIcon.addEventListener('click', () => {showHide(password,passIcon)})
-checkIcon.addEventListener('click', () => {showHide(passwordCheck,checkIcon)})
+passIcon.addEventListener('click', () => showHide(password,passIcon))
+checkIcon.addEventListener('click', () => showHide(passwordCheck,checkIcon))
 
 // 이메일 유효성 검사
-const input = document.querySelector('input');
-const userEmail = document.getElementById('user-email');
-const failureMessage = document.querySelector('.failure-message');
-const failureMessageTwo = document.querySelector('.failure-message2');
-const pattern = /^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]{2,3}$/;
+import { pattern } from '../components/regex.js'
+import { failureMessage, failureMessageTwo, input, userEmail } from '../components/emailValid.js';
 
 userEmail.addEventListener('focusout', () => {
   if (userEmail.value === '') {
-    failureMessage.classList.remove('hide');
     failureMessageTwo.classList.add('hide');
+    failureMessage.classList.remove('hide');
     input.classList.add('invalid-value');
     input.classList.remove('valid-value');
   } else if (!pattern.test(userEmail.value)) {      
@@ -37,43 +23,32 @@ userEmail.addEventListener('focusout', () => {
     input.classList.remove('valid-value');
   }
 });
-
 userEmail.addEventListener('keyup', () => {
   if (pattern.test(userEmail.value)) {
       failureMessage.classList.add('hide');
       failureMessageTwo.classList.add('hide');
-      userEmail.classList.remove('invalid-value');
       userEmail.classList.add('valid-value');
+      userEmail.classList.remove('invalid-value');
     }
 });
- 
-// 이메일 포커스
-userEmail.addEventListener('focusin', () => {
-  input.classList.add('blue-border');
-})
-
 
 //닉네임 유효성 검사
-const nickName = document.querySelector('#user-nickname')
-const nickNamePattern = /^(?=.*[a-z0-9가-힣])[a-z0-9가-힣]{2,}$/;
+import { nickNamePattern } from '../components/regex.js';
+const nickName = document.querySelector('#user-nickname');
 
-nickName.addEventListener('keyup', () => {
-  if (nickNamePattern.test(nickName.value)) {
-    nickName.classList.remove('invalid-value');
+function nickNameValid () {
+  if (nickNamePattern.test(nickName.value.trim())) {
     nickName.classList.add('valid-value');
-  } else if (!nickNamePattern.test(nickName.value)) {
+    nickName.classList.remove('invalid-value'); 
+  } else {
     nickName.classList.remove('valid-value');
     nickName.classList.add('invalid-value');
   }
-})
+};
+nickName.addEventListener('keyup', nickNameValid);
+nickName.addEventListener('focusout', nickNameValid);
 
-//닉네임 포커스
-nickName.addEventListener('focusin', () => {
-  nickName.classList.add('blue-border');
-})
-
-
-// 비밀버호 유효성 검사
+// 비밀번호 유효성 검사
 const failureMessageThree = document.querySelector('.failure-message3');
 const failureMessageFour = document.querySelector('.failure-message4');
 
@@ -99,40 +74,43 @@ password.addEventListener('keyup', () => {
       password.classList.add('valid-value');
     } 
 });
- 
-// 비밀번호 포커스
-password.addEventListener('focusin', () => {
-  password.classList.add('blue-border');
-});
 
-
-//비밀번호 확인 포커스
-passwordCheck.addEventListener('focusin', () => {
-  passwordCheck.classList.add('blue-border');
-});
-
-//비밀번호 확인 유효성 검사
+// 비밀번호 확인 유효성 검사
 const failureMessageFive = document.querySelector('.failure-message5');
 
-passwordCheck.addEventListener('keyup', () => {
-  if (passwordCheck.value.trim() === password.value.trim() && passwordCheck.value.length >= 8) {
+function passwordValid () {
+  if ((passwordCheck.value.trim() === password.value.trim()) && passwordCheck.value.length >= 8){
     failureMessageFive.classList.add('hide');
     passwordCheck.classList.remove('invalid-value');
     passwordCheck.classList.add('valid-value');
-  } else {
+  } else if ((passwordCheck.value.trim() !== password.value.trim()) || passwordCheck.value === '') {
+    failureMessageFive.classList.remove('hide');
+    passwordCheck.classList.add('invalid-value');
+    passwordCheck.classList.remove('valid-value');
+  } else if (passwordCheck.value === password.value && passwordCheck.value.length < 8) {
+    failureMessageFive.classList.add('hide');
+    passwordCheck.classList.add('invalid-value');
+    passwordCheck.classList.remove('valid-value');
+  }
+}
+
+function passwordValid2 () {
+  if ((passwordCheck.value.trim() === password.value.trim()) && passwordCheck.value.length >= 8){
+    failureMessageFive.classList.add('hide');
+    passwordCheck.classList.remove('invalid-value');
+    passwordCheck.classList.add('valid-value');
+  } else if ((passwordCheck.value.trim() !== password.value.trim()) && passwordCheck.value !== '') {
     failureMessageFive.classList.remove('hide');
     passwordCheck.classList.add('invalid-value');
     passwordCheck.classList.remove('valid-value');
   }
-})
+}
+password.addEventListener('keyup', passwordValid2);
+passwordCheck.addEventListener('keyup', passwordValid);
+passwordCheck.addEventListener('focusout', passwordValid);
 
-//버튼 활성화
+// 버튼 활성화
 const signupBtn = document.querySelector('.signup-btn');
-
-userEmail.addEventListener('keyup', btnActivate);
-password.addEventListener('keyup', btnActivate);
-nickName.addEventListener('keyup', btnActivate);
-passwordCheck.addEventListener('keyup', btnActivate);
 
 function btnActivate() {
   switch (passwordCheck.value.trim() === password.value.trim() && password.value.length >= 8 && nickNamePattern.test(nickName.value) && pattern.test(userEmail.value)) {
@@ -148,27 +126,33 @@ function btnActivate() {
   }
 }
 
-// 유저 데이터 베이스, 모달
-const USER_DATA = [
-  { email: 'codeit1@codeit.com', password: 'codeit101!' },
-  { email: 'codeit2@codeit.com', password: 'codeit202!' },
-  { email: 'codeit3@codeit.com', password: 'codeit303!' },
-  { email: 'codeit4@codeit.com', password: 'codeit404!' },
-  { email: 'codeit5@codeit.com', password: 'codeit505!' },
-  { email: 'codeit6@codeit.com', password: 'codeit606!' },
-];
+password.addEventListener('keyup', btnActivate);
+nickName.addEventListener('keyup', btnActivate);
+userEmail.addEventListener('keyup', btnActivate);
+passwordCheck.addEventListener('keyup', btnActivate);
 
+// 유저 데이터 베이스, 모달
 const modalCloseButton = document.getElementById('modalCloseButton');
 const modal = document.getElementById('modalContainer');
+import { USER_DATA } from '../components/userData.js';
 
 signupBtn.addEventListener('click', () => {
   const user = USER_DATA.find(userData => userData.email === userEmail.value)
   if (!user) {
-    window.location.href = 'login.html'
+    window.location.href = '../login/index.html'
   } else {
     modal.classList.remove('hidden')  
   }
 })
+
 modalCloseButton.addEventListener('click', () => {
   modal.classList.add('hidden')
 })
+
+// 인풋 포커스인
+import focusIn from '../components/focus.js';
+
+userEmail.addEventListener('focusin', () => focusIn(input));
+nickName.addEventListener('focusin', () => focusIn(nickName));
+password.addEventListener('focusin', () => focusIn(password));
+passwordCheck.addEventListener('focusin', () => focusIn(passwordCheck));
