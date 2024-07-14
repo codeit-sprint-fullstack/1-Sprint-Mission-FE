015d3f28e9963c542c8b4a601edf0bb68be39fe5
sohee <s2h110@naver.com>
2024-07-14 16:49:22 +0900

    showModal 함수 모듈 오류 수정

--- Javascript/showModal.mjs
+++ Javascript/showModal.mjs
@@ -1,7 +1,7 @@
     
-export function showModal(message) {
+export default function showModal(message) {
     let modal = document.getElementById("myModal");
-    
+
     document.getElementById('message').innerText = message;
     modal.style.display = "flex"; // 모달 창 표시
 }
