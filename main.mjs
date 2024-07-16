e21a3f325420b1fc40c7866ece9d5d657f26728f
juyeongeun <80696592+juyeongeun@users.noreply.github.com>
2024-07-16 13:51:32 +0900

    feat: productService

--- main.mjs
+++ main.mjs
@@ -6,4 +6,10 @@ ArticleService.getArticleList(1, 100, '후기');
 ArticleService.getArticle(3);
 ArticleService.postArticle('New Post', 'New Post Content', 'String');
 ArticleService.patchArticle(41, { content: 'Good' });
-// ArticleService.deleteArticle(43);
+ArticleService.deleteArticle(43);
+
+ProductService.getProductList(1, 10, '애플');
+ProductService.getProduct(3);
+ProductService.postProduct('초콜릿', '두바이 초콜릿', 100000, ['초콜릿', '간식'], ['url']);
+ProductService.patchProduct(17, { price: 110000 });
+ProductService.deleteProduct(17);
--- services/ProductService.mjs
+++ services/ProductService.mjs
@@ -1,3 +1,61 @@
 import axios from 'axios';
 
 const baseUrl = 'https://sprint-mission-api.vercel.app/products';
+
+// Get products
+export async function getProductList(page=1, pageSize=100, keyword) {
+    try {
+        const response = await axios.get(baseUrl, {
+            params: { page, pageSize, keyword }
+        });
+        console.log(response.data);
+    }catch(error) {
+        console.error('Get ProductList Error',error);
+    }
+}
+
+// Get a product
+export async function getProduct(id) {
+    try {
+        const response = await axios.get(`${baseUrl}/${id}`);
+        console.log(response.data);
+    }catch(error) {
+        console.error('Get Product Error', error);
+    }
+}
+
+// Post a product
+export async function postProduct(name,  description, price, tags, images) {
+    try {
+        const response = await axios.post(baseUrl, {
+            name,
+            description,
+            price,
+            tags,
+            images
+        });
+        console.log(response.data);
+    }catch(error) {
+        console.error('Post Product Error', error);
+    }
+}
+
+// PATCH 메서드를 사용하여 제품 수정하기
+export async function patchProduct(id, updates) {
+    try {
+        const response = await axios.patch(`${baseUrl}/${id}`, updates);
+        console.log(response.data);
+    } catch (error) {
+        console.error('Error updating product:', error);
+    }
+}
+
+// DELETE 메서드를 사용하여 제품 삭제하기
+export async function deleteProduct(id) {
+    try {
+        const response = await axios.delete(`${baseUrl}/${id}`);
+        console.log(response.data);
+    } catch (error) {
+        console.error('Error deleting product:', error);
+    }
+}
