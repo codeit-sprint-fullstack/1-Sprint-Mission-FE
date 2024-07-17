c5f2978834a7821652ffb9f9d251347668b626eb
juyeongeun <80696592+juyeongeun@users.noreply.github.com>
2024-07-18 08:52:20 +0900

    chore: 주석

---
+++ main.js
@@ -0,0 +1,15 @@
+// Importing Api
+import * as ProductService from './services/ProductService.js';
+import * as ArticleService from './services/ArticleService.js';
+
+ArticleService.getArticleList(1, 100, '후기');
+ArticleService.getArticle(10);
+ArticleService.postArticle('New Post', 'New Post Content', 'String');
+ArticleService.patchArticle(53, { content: 'Good' });
+ArticleService.deleteArticle(43);
+
+ProductService.getProductList(1, 10, '애플');
+ProductService.getProduct(66);
+ProductService.postProduct('초콜릿', '두바이 초콜릿', 100000, ['초콜릿', '간식'], ['url']);
+ProductService.patchProduct(17, { price: 110000 });
+ProductService.deleteProduct(17);
