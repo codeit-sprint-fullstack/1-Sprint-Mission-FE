d31deb016b3bf95c93a565f022a10c6dbcddbe12
mozzi34 <160555885+mozzi34@users.noreply.github.com>
2024-07-18 11:26:17 +0900

    Revert "js파일: 통일감을 위해 따옴표 통일,"
    This reverts commit be12b501a762c7d47b0fef702ce33078b139636d.

--- js/login.js
+++ js/login.js
@@ -1,71 +1,57 @@
-import {
-  emailForm,
-  userData,
-  showPasswordDialog,
-  okCLick,
-  userEmailBorder,
-  pswShow,
-  pswEmg,
-  showEmailDialog,
-} from './login_signup.js';
+import { emailForm, userData, showDialog, okCLick, userEmailBorder, pswShow, pswEmg, showDialog2 } from './login_signup.js';
 
 const showPw = document.querySelector('#show_pw');
-const userEmail = document.querySelector('#user_email');
-const emailMsg1 = document.querySelector('#emailMsg1');
-const emailMsg2 = document.querySelector('#emailMsg2');
-const userPassword = document.querySelector('#user_password');
-const pswdMsg1 = document.querySelector('#pswdMsg1');
-const pswdMsg2 = document.querySelector('#pswdMsg2');
+const userEmail = document.querySelector('#user_email')
+const emailMsg1 = document.querySelector("#emailMsg1")
+const emailMsg2 = document.querySelector("#emailMsg2")
+const userPassword = document.querySelector('#user_password')
+const pswdMsg1 = document.querySelector("#pswdMsg1")
+const pswdMsg2 = document.querySelector("#pswdMsg2")
 const loginBtn = document.querySelector('#login_btn');
-const ok = document.querySelector('#ok');
+const ok = document.querySelector("#ok")
 
-showPw.addEventListener('click', pswShow);
+showPw.addEventListener("click", pswShow)
 
-userEmail.addEventListener('blur', emailForm);
+userEmail.addEventListener("blur", emailForm);
 
-userEmail.addEventListener('click', userEmailBorder);
+userEmail.addEventListener("click", userEmailBorder);
 
-userPassword.addEventListener('blur', pswEmg);
+userPassword.addEventListener("blur", pswEmg);
 
-ok.addEventListener('click', okCLick);
+ok.addEventListener("click", okCLick);
 
-loginBtn.addEventListener('mouseover', function () {
-  if (
-    !(
-      userEmail.value &&
-      userPassword.value &&
-      emailMsg1.style.display === 'none' &&
-      emailMsg2.style.display === 'none' &&
-      pswdMsg1.style.display === 'none' &&
-      pswdMsg2.style.display === 'none'
-    )
-  ) {
+loginBtn.addEventListener("mouseover", function () {
+  if (!(userEmail.value && userPassword.value && emailMsg1.style.display === 'none' && emailMsg2.style.display === 'none' && pswdMsg1.style.display === 'none' && pswdMsg2.style.display === 'none')) {
     loginBtn.disabled = true;
   } else {
     loginBtn.disabled = false;
-    loginBtn.style.cursor = 'pointer';
+    loginBtn.style.cursor = "pointer"
   }
 });
 
-function navigateToItemPage() {
-  window.location.href = './item.html';
+function itemPage() {
+  window.location.href = './item.html'
 }
 
-loginBtn.addEventListener('click', function () {
+loginBtn.addEventListener("click", function () {
   for (let i = 0; i < userData.length; i++) {
     if (userEmail.value === userData[i].email) {
       if (userPassword.value === userData[i].password) {
-        navigateToItemPage();
+        itemPage();
         break;
       } else {
-        showPasswordDialog();
+        showDialog();
         break;
       }
-    } else {
+    }
+    else {
       if (i === userData.length - 1) {
-        showEmailDialog();
+        showDialog2();
         break;
       }
     }
   }
-});
+}
+)
+
+
--- js/login_signup.js
+++ js/login_signup.js
@@ -1,95 +1,84 @@
-export {
-  USER_DATA as userData,
-  emailForm,
-  pswShow,
-  pwShowSecond,
-  pswEmg,
-  showPasswordDialog,
-  okCLick,
-  userEmailBorder,
-  showEmailDialog,
-  showEmailCheckDialog,
-};
+export { USER_DATA as userData, emailForm, pswShow, pwShowSecond, pswEmg, showDialog, okCLick, userEmailBorder, showDialog2 }
 
 const mPassword = document.querySelector('.m_password');
 const mPasswordSecond = document.querySelector('.m_password_second');
 const showPw = document.querySelector('#show_pw');
 const showPwSecond = document.querySelector('#show_pw_second');
-const userPassword = document.querySelector('#user_password');
-const pswdMsg1 = document.querySelector('#pswdMsg1');
-const pswdMsg2 = document.querySelector('#pswdMsg2');
-const userEmail = document.querySelector('#user_email');
-const emailMsg1 = document.querySelector('#emailMsg1');
-const emailMsg2 = document.querySelector('#emailMsg2');
+const userPassword = document.querySelector('#user_password')
+const pswdMsg1 = document.querySelector("#pswdMsg1")
+const pswdMsg2 = document.querySelector("#pswdMsg2")
+const userEmail = document.querySelector('#user_email')
+const emailMsg1 = document.querySelector("#emailMsg1")
+const emailMsg2 = document.querySelector("#emailMsg2")
 const alertOverlay = document.querySelector('.alert_overlay');
-const dialogInnerText = document.querySelector('#dialog__inner_text');
+const dialogInnerText = document.querySelector('#dialog__inner_text')
 
 const USER_DATA = [
-  { email: 'codeit1@codeit.com', password: 'codeit101!' },
-  { email: 'codeit2@codeit.com', password: 'codeit202!' },
-  { email: 'codeit3@codeit.com', password: 'codeit303!' },
-  { email: 'codeit4@codeit.com', password: 'codeit404!' },
-  { email: 'codeit5@codeit.com', password: 'codeit505!' },
-  { email: 'codeit6@codeit.com', password: 'codeit606!' },
+  { email: 'codeit1@codeit.com', password: "codeit101!" },
+  { email: 'codeit2@codeit.com', password: "codeit202!" },
+  { email: 'codeit3@codeit.com', password: "codeit303!" },
+  { email: 'codeit4@codeit.com', password: "codeit404!" },
+  { email: 'codeit5@codeit.com', password: "codeit505!" },
+  { email: 'codeit6@codeit.com', password: "codeit606!" },
 ];
 
 function emailForm() {
-  let emailText =
-    /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
-  if (userEmail.value === '') {
-    emailMsg1.style.display = 'block';
+  let emailText = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
+  if (userEmail.value === "") {
+    emailMsg1.style.display = "block";
     userEmail.style.border = '1px solid #F74747';
     emailMsg2.style.display = 'none';
+
   } else {
-    emailMsg1.style.display = 'none';
+    emailMsg1.style.display = "none";
     userEmail.style.border = 'none';
     if (emailText.test(userEmail.value) == false) {
-      emailMsg2.style.display = 'block';
+      emailMsg2.style.display = "block";
       userEmail.style.border = '1px solid #F74747';
     } else {
-      emailMsg2.style.display = 'none';
+      emailMsg2.style.display = "none";
       userEmail.style.border = 'none';
     }
   }
-}
+};
 
 function pswShow() {
-  if (mPassword.type === 'password') {
-    mPassword.type = 'text';
-    showPw.classList.remove('fa-eye-slash');
-    showPw.classList.add('fa-eye');
+  if (mPassword.type === "password") {
+    mPassword.type = "text";
+    showPw.classList.remove("fa-eye-slash");
+    showPw.classList.add("fa-eye");
   } else {
-    mPassword.type = 'password';
-    showPw.classList.remove('fa-eye');
-    showPw.classList.add('fa-eye-slash');
+    mPassword.type = "password";
+    showPw.classList.remove("fa-eye");
+    showPw.classList.add("fa-eye-slash");
   }
-}
+};
 
 function pwShowSecond() {
-  if (mPasswordSecond.type === 'password') {
-    mPasswordSecond.type = 'text';
-    showPwSecond.classList.remove('fa-eye-slash');
-    showPwSecond.classList.add('fa-eye');
+  if (mPasswordSecond.type === "password") {
+    mPasswordSecond.type = "text";
+    showPwSecond.classList.remove("fa-eye-slash");
+    showPwSecond.classList.add("fa-eye");
   } else {
-    mPasswordSecond.type = 'password';
-    showPwSecond.classList.remove('fa-eye');
-    showPwSecond.classList.add('fa-eye-slash');
+    mPasswordSecond.type = "password";
+    showPwSecond.classList.remove("fa-eye");
+    showPwSecond.classList.add("fa-eye-slash");
   }
 }
 
 function pswEmg() {
-  if (userPassword.value === '') {
-    pswdMsg1.style.display = 'block';
+  if (userPassword.value === "") {
+    pswdMsg1.style.display = "block";
     userPassword.style.border = '1px solid #F74747';
-    pswdMsg2.style.display = 'none';
+    pswdMsg2.style.display = "none";
   } else {
-    pswdMsg1.style.display = 'none';
+    pswdMsg1.style.display = "none";
     userPassword.style.border = 'none';
     if (userPassword.value.length < 8) {
-      pswdMsg2.style.display = 'block';
+      pswdMsg2.style.display = "block";
       userPassword.style.border = '1px solid #F74747';
     } else {
-      pswdMsg2.style.display = 'none';
+      pswdMsg2.style.display = "none";
       userPassword.style.border = 'none';
     }
   }
@@ -97,22 +86,19 @@ function pswEmg() {
 
 function userEmailBorder() {
   userEmail.style.border = 'none';
-}
+};
 
-function showPasswordDialog(event) {
+function showDialog(event) {
   alertOverlay.style.display = 'block';
   dialogInnerText.textContent = '비밀번호가 일치하지 않습니다.';
   event.preventDefault();
 }
-function showEmailDialog(event) {
+function showDialog2(event) {
   alertOverlay.style.display = 'block';
   dialogInnerText.textContent = '이메일이 일치하지 않습니다.';
   event.preventDefault();
 }
-
-function showEmailCheckDialog(event) {
-  alertOverlay.style.display = 'block';
-}
 function okCLick() {
   alertOverlay.style.display = 'none';
-}
+};
+
