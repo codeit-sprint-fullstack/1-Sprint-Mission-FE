29043a21d0d1b3825cc42582e9f0d8cba11bbe5a
sohee <s2h110@naver.com>
2024-07-18 23:34:48 +0900

    Modified import path to reflect updated filename in productService.js

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-import { getProductList, getProduct, createProduct, patchProduct, deleteProduct } from "./product-service.js";
-import { checkStatus, getArticle, getArticleList, createArticle,patchArticle,deleteArticle } from "./article-service.js";
+import { getProductList, getProduct, createProduct, patchProduct, deleteProduct } from "./productService.js";
+import { checkStatus, getArticle, getArticleList, createArticle,patchArticle,deleteArticle } from "./articleService.js";
 
 
 const newProduct = {
-    "name": "텀블러",
+    "name": "텀블러", 
     "description": "스타벅스 텀블러",
     "price": 15000,
     "manufacturer": "manufacturer",
@@ -26,7 +26,7 @@ const newProduct = {
 /* ----------ProductService.js 파일 함수 동작 확인----------- */
 
 // 특정 상품 정보 가져오기
-//getProduct(3);
+getProduct(5);
 
 //상품 목록 가져오기
 //getProductList(1, 1, '의자');
