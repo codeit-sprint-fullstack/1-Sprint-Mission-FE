8d98ba594e684dfdc57cdff674d0a608f1bd1f07
mozzi34 <160555885+mozzi34@users.noreply.github.com>
2024-07-18 11:50:34 +0900

    edit: js파일 따옴표 통일/ 로그인,회원가입 팝업 이름 직관적으로 변경(showDialog -> showEmailDialog, showPasswordDialog,showEmailCheckDialog)

--- js/login.js
+++ js/login.js
@@ -1,57 +1,71 @@
-import { emailForm, userData, showDialog, okCLick, userEmailBorder, pswShow, pswEmg, showDialog2 } from './login_signup.js';
+import {
+  emailForm,
+  userData,
+  okCLick,
+  userEmailBorder,
+  pswShow,
+  pswEmg,
+  showEmailDialog,
+  showPasswordDialog,
+} from './login_signup.js';
 
 const showPw = document.querySelector('#show_pw');
-const userEmail = document.querySelector('#user_email')
-const emailMsg1 = document.querySelector("#emailMsg1")
-const emailMsg2 = document.querySelector("#emailMsg2")
-const userPassword = document.querySelector('#user_password')
-const pswdMsg1 = document.querySelector("#pswdMsg1")
-const pswdMsg2 = document.querySelector("#pswdMsg2")
+const userEmail = document.querySelector('#user_email');
+const emailMsg1 = document.querySelector('#emailMsg1');
+const emailMsg2 = document.querySelector('#emailMsg2');
+const userPassword = document.querySelector('#user_password');
+const pswdMsg1 = document.querySelector('#pswdMsg1');
+const pswdMsg2 = document.querySelector('#pswdMsg2');
 const loginBtn = document.querySelector('#login_btn');
-const ok = document.querySelector("#ok")
+const ok = document.querySelector('#ok');
 
-showPw.addEventListener("click", pswShow)
+showPw.addEventListener('click', pswShow);
 
-userEmail.addEventListener("blur", emailForm);
+userEmail.addEventListener('blur', emailForm);
 
-userEmail.addEventListener("click", userEmailBorder);
+userEmail.addEventListener('click', userEmailBorder);
 
-userPassword.addEventListener("blur", pswEmg);
+userPassword.addEventListener('blur', pswEmg);
 
-ok.addEventListener("click", okCLick);
+ok.addEventListener('click', okCLick);
 
-loginBtn.addEventListener("mouseover", function () {
-  if (!(userEmail.value && userPassword.value && emailMsg1.style.display === 'none' && emailMsg2.style.display === 'none' && pswdMsg1.style.display === 'none' && pswdMsg2.style.display === 'none')) {
+loginBtn.addEventListener('mouseover', function () {
+  if (
+    !(
+      userEmail.value &&
+      userPassword.value &&
+      emailMsg1.style.display === 'none' &&
+      emailMsg2.style.display === 'none' &&
+      pswdMsg1.style.display === 'none' &&
+      pswdMsg2.style.display === 'none'
+    )
+  ) {
     loginBtn.disabled = true;
   } else {
     loginBtn.disabled = false;
-    loginBtn.style.cursor = "pointer"
+    loginBtn.style.cursor = 'pointer';
   }
 });
 
 function itemPage() {
-  window.location.href = './item.html'
+  window.location.href = './item.html';
 }
 
-loginBtn.addEventListener("click", function () {
+loginBtn.addEventListener('click', function () {
   for (let i = 0; i < userData.length; i++) {
     if (userEmail.value === userData[i].email) {
       if (userPassword.value === userData[i].password) {
         itemPage();
         break;
       } else {
-        showDialog();
+        showPasswordDialog();
         break;
       }
-    }
-    else {
+    } else {
       if (i === userData.length - 1) {
-        showDialog2();
+        showEmailDialog();
         break;
       }
     }
   }
-}
-)
-
-
+});
