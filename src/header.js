14f46011143789f1f00b8403e5aab6e71e881f00
minsu <a2238737@gmail.com>
2024-07-23 18:21:41 +0900

    Feat: React file add

---
+++ src/header.js
@@ -0,0 +1,12 @@
+function Headers(){
+  return(
+    <div id="header" style={ { display: 'flex', flexDirection: 'row' , width: '100%'}}>
+      <img id="header_img"src=""></img>
+      <p>자유게시판</p>
+      <p>중고마켓</p>
+      <img id="header_my_img"src=""></img>
+    </div>
+  )
+}
+
+export default Headers
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import Headers from './header';
+
+ReactDOM.render(
+  <Headers />,
+  document.getElementById('root')
+);
