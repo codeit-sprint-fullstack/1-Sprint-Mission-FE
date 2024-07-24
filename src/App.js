bd0de27020eb452fd83cda31232f3cc79e8037bd
jongmin <charlieko123@github.com>
2024-07-24 23:13:52 +0900

    initialize file

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome to My React App</h1>
+        <p>This is a simple React application.</p>
       </header>
     </div>
   );
