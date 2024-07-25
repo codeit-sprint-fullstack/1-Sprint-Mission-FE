004f2f6170b5e245afe4bd492d18bc7ecfce94cd
jongmin <charlieko123@github.com>
2024-07-25 11:22:55 +0900

    add nav style

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,16 +1,19 @@
 import React from "react";
 import logo from "../images/pandaLogo.png";
 import icon from "../images/userIcon.png";
+import "../styles/Navigation.css";
 
 function Navigation() {
   return (
-    <nav>
-      <img src={logo} alt="Home Logo" />
-      <div>
-        <span>자유게시판</span>
-        <span>중고마켓</span>
+    <nav id="nav-bar">
+      <div className="nav-left">
+        <img className="logo" src={logo} alt="Home Logo" />
+        <div className="menu Text-2lg Bold">
+          <span className="menu-content">자유게시판</span>
+          <span className="menu-content">중고마켓</span>
+        </div>
       </div>
-      <img src={icon} alt="User Icon" />
+      <img className="icon" src={icon} alt="User Icon" />
     </nav>
   );
 }
