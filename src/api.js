92cebb191393b9b1f84081b4472f7870b8ab7265
jongmin <charlieko123@github.com>
2024-07-26 00:00:34 +0900

    add search-product

--- src/api.js
+++ src/api.js
@@ -1,9 +1,14 @@
 const URL = "https://panda-market-api.vercel.app/products";
 
-export const getItems = async (page = 1, pageSize = 10, order = "favorite") => {
+export const getItems = async (
+  page = 1,
+  pageSize = 10,
+  order = "favorite",
+  keyword = ""
+) => {
   try {
     const res = await fetch(
-      `${URL}?page=${page}&pageSize=${pageSize}&orderBy=${order}`
+      `${URL}?page=${page}&pageSize=${pageSize}&orderBy=${order}&keyword=${keyword}`
     );
     if (!res.ok) {
       throw new Error(res.status);
--- src/components/SaleProducts.js
+++ src/components/SaleProducts.js
@@ -3,6 +3,7 @@ import { getItems } from "../api.js";
 import Product from "./Product.js";
 import Footer from "./Footer.js";
 import "../styles/SaleProducts.css";
+import searchIcon from "../images/searchIcon.png";
 
 function SaleProducts() {
   const [products, setProducts] = useState([]);
@@ -11,20 +12,27 @@ function SaleProducts() {
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPage, setTotalPage] = useState(1);
 
+  const [keyword, setKeyword] = useState("");
+
   const PAGE_SIZE = 10;
 
   useEffect(() => {
     const getProducts = async () => {
-      const data = await getItems(currentPage, PAGE_SIZE, order);
+      const data = await getItems(currentPage, PAGE_SIZE, order, keyword);
       setProducts(data.list);
       setTotalPage(Math.ceil(data.totalCount / PAGE_SIZE));
     };
 
     getProducts();
-  }, [order, currentPage]);
+  }, [order, currentPage, keyword]);
 
   const handleChangePage = (page) => {
     setCurrentPage(page);
+  };
+
+  const handleSearch = (event) => {
+    setKeyword(event.target.value);
+    setCurrentPage(1);
   };
 
   return (
@@ -32,7 +40,14 @@ function SaleProducts() {
       <div className="sale-product-nav">
         <h2 className="sale-product-title Text-xl Bold">판매중인 상품</h2>
         <div className="nav-options">
-          <div>검색창</div>
+          <input
+            className="search-product"
+            type="text"
+            placeholder="검색할 상품을 입력해주세요"
+            value={keyword}
+            onChange={handleSearch}
+          />
+          <img src={searchIcon} alt="search icon" className="search-icon" />
           <div>상품등록하기</div>
           <div>정렬선택하기</div>
         </div>
