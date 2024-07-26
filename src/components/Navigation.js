c56583bf4afde6622fe8499c9fa021d74bdcc1da
jongmin <charlieko123@github.com>
2024-07-26 10:36:29 +0900

    add home logo <a>

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -7,7 +7,9 @@ function Navigation() {
   return (
     <nav className="nav-bar">
       <div className="nav-left">
-        <img className="logo" src={logo} alt="Home Logo" />
+        <a href="/" target="_self">
+          <img className="logo" src={logo} alt="Home Logo" />
+        </a>
         <div className="menu Text-2lg Bold">
           <span className="menu-content">자유게시판</span>
           <span className="menu-content">중고마켓</span>
