47e6627306cd65649ca1ca6720a0aff3dd5fa4dd
DESKTOP-EHH0K4D\SnowRang <ajantang@naver.com>
2024-07-31 11:40:08 +0900

    feat : add profile img instead of btn_login
    요구사항에 맞춰 로그인 버튼 프로필 이미지로 변경
    미션 1,2,3의 figma가 변경되어 원본 디자인이 남아 있지 않지만, 이전 미션의 디자인과 99.9% 동일하게 만든걸 생각하면,
    미션 5에서 header의 nav 부분의 디자인이 변경되어 있는 것을 확인
    단, 요구사항에 미션 1,2,3과 동일한 스타일과 규칙을 요구 했기 때문에 프로필 이미지만 교체

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,10 @@
 import "../assets/styles/header.css";
+import tempProfileImg from "../assets/images/ic_profile40.svg";
 
 export function Header() {
+  const tempIsSignin = true;
+  // const tempProfileImg = "../assets/images/ic_profile40.svg";
+
   return (
     <header>
       <a className="header__btn-home-frame" href="/" target="_self">
@@ -14,11 +18,19 @@ export function Header() {
           <li className="Text-2lg Bold header__list-item">중고마켓</li>
         </ul>
       </nav>
-      <a className="header__btn-login-frame" href="/login" target="_self">
-        <span>
-          <img className="header__img-login" alt="로그인 버튼" />
-        </span>
-      </a>
+      {tempIsSignin ? (
+        <img
+          className="header__btn-profile"
+          src={tempProfileImg}
+          alt="프로필 그림"
+        />
+      ) : (
+        <a className="header__btn-login-frame" href="/login" target="_self">
+          <span>
+            <img className="header__img-login" alt="로그인 버튼" />
+          </span>
+        </a>
+      )}
     </header>
   );
 }
