be7fec9f13c7a000d093d2b37edaf4dbb0481a75
minseokim <claudia503@naver.com>
2024-07-31 18:30:38 +0900

    feat: mission5_Navbar.js 모바일 버전 로고 지정

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Navbar.css';
 
 const Navbar = () => {
+  const [logoSrc, setLogoSrc] = useState('image/logo.svg');
+
+  useEffect(() => {
+    const updateLogo = () => {
+      if (window.innerWidth <= 743) {
+        setLogoSrc('image/mobile_logo.svg');
+      } else {
+        setLogoSrc('image/logo.svg');
+      }
+    };
+
+    window.addEventListener('resize', updateLogo);
+    updateLogo(); // 초기 로고 설정
+
+    return () => {
+      window.removeEventListener('resize', updateLogo);
+    };
+  }, []);
+
   return (
     <nav className="navbar">
-      <img src="image/logo.svg" alt="Panda" className="panda" />
+      <img src={logoSrc} alt="Panda" className="panda" />
       <div className="nav-links">
         <a href="/" className="board-link">자유게시판</a>
         <a href="/" className="market-link">중고마켓</a>
@@ -15,3 +34,4 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
