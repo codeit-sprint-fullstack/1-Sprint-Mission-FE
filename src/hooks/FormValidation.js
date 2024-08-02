8090c12d079b5161567fe5f7b5fd71794663510a
twh <owootak@naver.com>
2024-08-02 20:37:46 +0900

    인풋 유효성 검사 추가

---
+++ src/hooks/FormValidation.js
@@ -0,0 +1,72 @@
+import { useState } from "react";
+
+const validateField = (name, value) => {
+  switch (name) {
+    case "name":
+      return value.length >= 1 && value.length <= 10
+        ? ""
+        : "상품명은 1자리 이상 10자리 이내 입니다.";
+    case "decription":
+      return value.length >= 10 && value.length <= 100
+        ? ""
+        : "상품 소개는 10자리 이상 100자리 이내 입니다.";
+    case "price":
+      return value > 0 ? "" : "판매 가격은 1원 이상입니다.";
+    case "tag":
+      return value.every((item) => item.length <= 5)
+        ? ""
+        : "각 태그는 5자리 이내 입니다.";
+    default:
+      return "";
+  }
+};
+
+const useFormValidation = (initialValues) => {
+  const [values, setValues] = useState(initialValues);
+  const [errors, setErrors] = useState({});
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setValues({
+      ...values,
+      [name]: value,
+    });
+
+    // 유효성 검사
+    const error = validateField(name, value);
+    setErrors({
+      ...errors,
+      [name]: error,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    // 모든 필드 유효성 검사
+    const newErrors = {};
+    let hasErrors = false;
+
+    Object.keys(values).forEach((key) => {
+      const error = validateField(key, values[key]);
+      if (error) {
+        newErrors[key] = error;
+        hasErrors = true;
+      }
+    });
+
+    if (hasErrors) {
+      setErrors(newErrors);
+      return;
+    }
+  };
+
+  return {
+    values,
+    errors,
+    handleChange,
+    handleSubmit,
+  };
+};
+
+export default useFormValidation;
--- src/pages/Registration.js
+++ src/pages/Registration.js
@@ -1,5 +1,65 @@
-function Registration() {
-  return <div></div>;
-}
+import React from "react";
+import useFormValidation from "../hooks/FormValidation.js";
 
+function Registration() {
+  const { values, errors, handleChange, handleSubmit } = useFormValidation({
+    email: "",
+    password: "",
+    username: "",
+  });
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>삼품명</label>
+        <input
+          type="text"
+          name="name"
+          value={values.name || ""}
+          onChange={handleChange}
+        />
+        {/* 유효성 검사 메시지 표시 */}
+        {errors.name && <p style={{ color: "red" }}>{errors.name}</p>}
+      </div>
+
+      <div>
+        <label>상품 소개</label>
+        <input
+          type="text"
+          name="decription"
+          value={values.decription || ""}
+          onChange={handleChange}
+        />
+        {/* 유효성 검사 메시지 표시 */}
+        {errors.decription && <p style={{ color: "red" }}>{errors.password}</p>}
+      </div>
+
+      <div>
+        <label>판매가격</label>
+        <input
+          type="text"
+          name="price"
+          value={values.price || ""}
+          onChange={handleChange}
+        />
+        {/* 유효성 검사 메시지 표시 */}
+        {errors.price && <p style={{ color: "red" }}>{errors.price}</p>}
+      </div>
+
+      <div>
+        <label>태그</label>
+        <input
+          type="text"
+          name="tag"
+          value={values.tag || ""}
+          onChange={handleChange}
+        />
+        {/* 유효성 검사 메시지 표시 */}
+        {errors.tag && <p style={{ color: "red" }}>{errors.tag}</p>}
+      </div>
+
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
 export default Registration;
