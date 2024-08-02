531acf1285791c5161d5528c3d935f0ee1f8cbb9
root <root@DESKTOP-UE7169K>
2024-08-02 15:38:03 +0900

    node-module 추가

--- react-mission/src/components/Nav.js
+++ react-mission/src/components/Nav.js
@@ -1,15 +1,15 @@
-import userIcon from './img/userIcon.png'
+import userIcon from "./img/userIcon.png";
 
 function Nav() {
-    return (
-        <nav>
-            <div className='listContaner'>
-                <p className='listFont freeFont'>자유게시판</p>
-                <p className='listFont usedFont'>중고마켓</p>
-            </div>
-            <img className='userIcon' src={userIcon} alt='유저 아이콘'/>   
-        </nav>
-    );
+  return (
+    <nav>
+      <div className="listContaner">
+        <p className="listFont freeFont">자유게시판</p>
+        <p className="listFont usedFont">중고마켓</p>
+      </div>
+      <img className="userIcon" src={userIcon} alt="유저 아이콘" />
+    </nav>
+  );
 }
 
 export default Nav;
