cdb614cb0451f349dbddfd8742949cf4b5529c39
kangbeomjoon <kobe7698@gmail.com>
2024-08-04 23:15:54 +0900

    ProductList.js & api.js 수정

--- react-sprint-mission5/src/api.js
+++ react-sprint-mission5/src/api.js
@@ -3,12 +3,24 @@ import axios from "axios";
 
 const API_URL = "https://panda-market-api.vercel.app/api/products";
 
-export const fetchProducts = async () => {
+export const fetchProducts = async ({
+  page = 1,
+  pageSize = 10,
+  sortBy = "createdAt",
+  keyword = "",
+}) => {
+  const params = new URLSearchParams({
+    page,
+    pageSize,
+    sortBy,
+    keyword,
+  });
+
   try {
-    const response = await axios.get(API_URL);
-    return response.data; // 데이터 반환
+    const response = await axios.get(`${API_URL}?${params.toString()}`);
+    return response.data;
   } catch (error) {
     console.error("Failed to fetch products:", error);
-    return []; // 에러 발생 시 빈 배열 반환
+    throw new Error("데이터를 불러오는데 실패했습니다."); // 사용자에게 보여줄 오류 메시지
   }
 };
--- react-sprint-mission5/src/components/ProductList.js
+++ react-sprint-mission5/src/components/ProductList.js
@@ -7,16 +7,30 @@ import Pagination from "./Pagination";
 
 function ProductList() {
   const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
   const [sort, setSort] = useState("createdAt");
-  const [currentPage, setCurrentPage] = useState(1); // 현재 페이지 상태 추가
-  const [totalPages, setTotalPages] = useState(1); // 총 페이지 수 상태 추가
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
   const pageSize = 10;
 
   useEffect(() => {
     const getProducts = async () => {
-      const fetchedProducts = await fetchProducts(currentPage, pageSize, sort);
-      setProducts(fetchedProducts.products);
-      setTotalPages(Math.ceil(fetchedProducts.totalCount / pageSize)); // 총 페이지 수 계산
+      setLoading(true);
+      setError(null);
+      try {
+        const fetchedProducts = await fetchProducts(
+          currentPage,
+          pageSize,
+          sort
+        );
+        setProducts(fetchedProducts.products);
+        setTotalPages(Math.ceil(fetchedProducts.totalCount / pageSize));
+        setLoading(false);
+      } catch (err) {
+        setError("상품을 불러오는 중 에러가 발생했습니다.");
+        setLoading(false);
+      }
     };
 
     getProducts();
@@ -24,12 +38,15 @@ function ProductList() {
 
   const handleSortChange = (event) => {
     setSort(event.target.value);
-    setCurrentPage(1); // 정렬 기준 변경 시 페이지를 1로 초기화
+    setCurrentPage(1);
   };
 
   const handlePageChange = (page) => {
-    setCurrentPage(page); // 페이지 상태 업데이트
+    setCurrentPage(page);
   };
+
+  if (loading) return <div>로딩 중...</div>;
+  if (error) return <div>{error}</div>;
 
   return (
     <div>
