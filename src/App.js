1b81912e31e005072f22376915cb5e32b8550aa2
jongmin <charlieko123@github.com>
2024-08-05 09:56:10 +0900

    use router

--- src/App.js
+++ src/App.js
@@ -2,19 +2,37 @@ import React from "react";
 import Navigation from "./components/Navigation";
 import BestProducts from "./components/BestProducts";
 import SaleProducts from "./components/SaleProducts";
+import Registration from "./components/Registration";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
       <div className="main-header">
         <Navigation />
       </div>
-      <div className="main-body">
-        <BestProducts />
-        <SaleProducts />
-      </div>
-    </div>
+      <Routes>
+        <Route
+          index
+          element={
+            <div className="main-body">
+              <BestProducts />
+              <SaleProducts />
+            </div>
+          }
+        />
+        <Route
+          path="/items"
+          element={
+            <div className="main-body">
+              <SaleProducts />
+            </div>
+          }
+        />
+        <Route path="/registration" element={<Registration />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Registration.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Registration() {
+  return <div>상품 등록하기</div>;
+}
+
+export default Registration;
