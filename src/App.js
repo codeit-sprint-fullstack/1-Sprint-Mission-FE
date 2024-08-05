d0564c90c628ce10be649b3ac2ff13ad3e41d089
jongmin <charlieko123@github.com>
2024-08-05 10:30:38 +0900

    add default page to items & delete best product

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navigation from "./components/Navigation";
-import BestProducts from "./components/BestProducts";
 import SaleProducts from "./components/SaleProducts";
 import Registration from "./components/Registration";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
 import "./App.css";
 
 function App() {
@@ -13,15 +12,7 @@ function App() {
         <Navigation />
       </div>
       <Routes>
-        <Route
-          index
-          element={
-            <div className="main-body">
-              <BestProducts />
-              <SaleProducts />
-            </div>
-          }
-        />
+        <Route index element={<NavigateToItems />} />
         <Route
           path="/items"
           element={
@@ -34,6 +25,16 @@ function App() {
       </Routes>
     </BrowserRouter>
   );
+}
+
+function NavigateToItems() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate("/items");
+  }, [navigate]);
+
+  return;
 }
 
 export default App;
