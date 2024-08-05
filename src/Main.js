4e8e5947bda2c20e4e601949bd6307bb29b7be78
sanho <dieuaimer@gmail.com>
2024-08-05 17:58:44 +0900

    pagination component 분리

--- src/Main.js
+++ src/Main.js
@@ -1,13 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from './components/App';
+import HomePage from "./pages/HomePage";
 
 
 function Main() {
   return (
     <BrowserRouter>
-        <Routes>
-          <Route path="items" element={<App />}></Route>
-        </Routes>
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="items" element={<App />}></Route>
+      </Routes>  
     </BrowserRouter>
   );
 
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import Nav from "../components/Nav";
+
+function HomePage() {
+  return (
+    <div className="HomePage">
+      <Nav/>
+      <div><h1>HOME</h1></div>
+    </div>
+  );
+}
+
+export default HomePage;
