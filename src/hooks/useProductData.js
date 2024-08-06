ff385d50870c4d1167a79a41fe5287cdc478ada4
Jun5 <chdh1005@gmail.com>
2024-08-06 13:24:18 +0900

    상품 리스트 렌더링 코드 및 이름 수정 > ProductGridRender.js

--- src/hooks/useProductData.js
+++ src/hooks/useProductData.js
@@ -4,30 +4,29 @@ import getApiData from "../api/getProductsData.js";
 
 const URL = "https://panda-market-api.vercel.app";
 
-const getProductData = (page, pageSize, orderBy, keyword, productsCount) => {
+const getProductData = (nowPage, showProductCount, orderBy, keyword) => {
 
-  const [getProductsList, setProductsList] = useState([]);
-  const [noProduct, setBoolProduct] = useState(false);
+  const [getProductList, setProductsList] = useState([]);
   const [totalPageSize, setTotalPageSize] = useState(1);
 
   useEffect(() => {
-    getApiData (page = 1, pageSize = 10, orderBy = 'recent', keyword = '')
+    getApiData (nowPage = 1, showProductCount = 10, orderBy = 'recent', keyword = '')
       .then((data) => {
         // 상품 최대 개수 확인
         console.log(`data.totalCount: ${data.totalCount}`);
         // 최대 페이지 계산
-        const maxPageCalc = Math.ceil(data.totalCount / productsCount);
+        const maxPageCalc = Math.ceil(data.totalCount / showProductCount);
         setTotalPageSize(maxPageCalc);
 
         // data 정보 콘솔 로그
         console.log(`data.list.length: ${data.list.length}`);
         // 상품 데이터 확인
         if (data.list.length === 0) { // 없으면 noProductList를 True로 설정
-          setBoolProduct(true);
-        } else if (data.list.length < productsCount) { // 상품이 보여줄 갯수보다 적으면 가데이터로 채우기
+          setProductsList(null)
+        } else if (data.list.length < showProductCount) { // 상품이 보여줄 갯수보다 적으면 가데이터로 채우기
           const newFiiledDataList = [
             ...data.list,
-            ...Array(productsCount - data.list.length).fill({
+            ...Array(showProductCount - data.list.length).fill({
               images: [search],
               name: "",
               description: "",
@@ -37,15 +36,14 @@ const getProductData = (page, pageSize, orderBy, keyword, productsCount) => {
           ];
           setProductsList(newFiiledDataList); 
         } else {
-          setProductsList(data.list); // 이상 없으면 getProductsList 채우기
+          setProductsList(data.list); // 이상 없으면 getProductList 채우기
         }
       })
       .catch((error) => console.error(error));
   }, []);
 
   return {
-    getProductsList,
-    noProduct,
+    getProductList,
     totalPageSize
   }
 
