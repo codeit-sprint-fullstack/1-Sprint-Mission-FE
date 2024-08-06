49c4078c17baaa3e8bef547d75539f9b0ba7ee23
jongmin <charlieko123@github.com>
2024-08-06 09:06:23 +0900

    rename params again

--- src/api.js
+++ src/api.js
@@ -1,9 +1,9 @@
 const URL = "https://one-sprint-mission-be.onrender.com";
 
-export const getItems = async (page = 1, limit = 10, search = "") => {
+export const getItems = async (page = 1, pageSize = 10, keyword = "") => {
   try {
     const res = await fetch(
-      `${URL}?page=${page}&limit=${limit}&search=${search}`
+      `${URL}?page=${page}&pageSize=${pageSize}&keyword=${keyword}`
     );
     if (!res.ok) {
       throw new Error(res.status);
