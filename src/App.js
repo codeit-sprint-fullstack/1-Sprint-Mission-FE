2c40d07ab8a6b6b0327e881307370bbb819331ec
S2OHEE <s2h110@naver.com>
2024-08-07 11:26:01 +0900

    상품 등록 페이지인 RegistrationPage 경로 추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom'; 
 import ItemsPage from './pages/ItemsPage';
+import RegistrationPage from './pages/RegistrationPage'; 
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/items" element={<ItemsPage />}></Route>
+        <Route path="/registration" element={<RegistrationPage />}></Route>
       </Routes>
     </div>
   );
