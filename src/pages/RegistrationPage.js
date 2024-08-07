7c77c7ff5cd612ec408593e90c70f0075320dcae
S2OHEE <s2h110@naver.com>
2024-08-07 11:44:32 +0900

    상품 등록을 위한 폼 추가

--- src/pages/RegistrationPage.js
+++ src/pages/RegistrationPage.js
@@ -8,3 +8,61 @@ const INITIAL_VALUES = {
   price: '',
   tags: '',
 };
+
+function RegistrationPage() {
+
+  return (
+    <div className='RegistrationPage'>
+      <ItemsPageHeader />
+      <div className="product-form">
+        <form>
+          <div className='FormTop'>
+            <h2>상품 등록하기</h2>
+            <button>등록</button>
+          </div>
+          <label className='Label1'>
+            상품명
+            <input
+              id='Input1'
+              className={`RegistrationInput`}
+              type="text"
+              name="name"
+              required
+            />
+          </label>
+          <label className='Label2'>
+            상품 소개
+            <textarea
+              id='Input2'
+              name="description"
+              className={`RegistrationInput`}
+              required
+            />
+          </label>
+          <label className='Label3'>
+            판매 가격
+            <input
+              id='Input3'
+              className={`RegistrationInpu`}
+              type="number"
+              name="price"
+              required
+            />
+          </label>
+          <label className='Label4'>
+            태그
+            <input
+              id='Input4'
+              className={`RegistrationInput`}
+              type="text"
+              name="tags"
+              required
+            />
+          </label>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default RegistrationPage;
