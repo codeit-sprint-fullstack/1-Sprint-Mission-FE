c3b684f8fa817d54eababa41f462b4658ab4679e
S2OHEE <s2h110@naver.com>
2024-08-07 16:09:31 +0900

    feat: useEffect 사용하여 입력 값 변경시, 유효성 검사 수행

--- src/hooks/useFormValidation.js
+++ src/hooks/useFormValidation.js
@@ -4,6 +4,10 @@ const useFormValidation = (initialValues) => {
   const [values, setValues] = useState(initialValues);
   const [errors, setErrors] = useState({});
 
+  useEffect(() => {
+    validate(); // 입력 값에 따라 유효성 검사 실시
+  }, [values]);
+
 };
 
 export default useFormValidation;
