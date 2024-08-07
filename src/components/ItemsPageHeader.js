85990cadbad018d45f2dfef457aa581ded28667b
S2OHEE <s2h110@naver.com>
2024-08-07 11:06:53 +0900

    ItemsPageHeader.js 추가하여 프로필이미지를 로그인 버튼으로 수 정

---
+++ src/components/ItemsPageHeader.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Header.css';
+import pandaLogo from '../assets/images/panda-face.png';
+import pandaMarket from '../assets/images/panda-market.png';
+
+/* 중고마켓 홈페이지 헤더 */
+function ItemsPageHeader() {
+  return (
+    <header className="header">
+      <div className="logo-container">
+        <img src={pandaLogo} alt="Panda Logo" className="logo" />
+        <img src={pandaMarket} alt="Panda Market" className="logo" id="pandaMarket_logo" />
+      </div>
+      <nav className="nav">
+        <ul>
+        <li><a href="#free-board">자유게시판</a></li>
+        <li><a id='ItemPageMarket' href="#market">중고마켓</a></li>
+        </ul>
+      </nav>
+      <button id='LoginButton'>로그인</button>
+    </header>
+  );
+}
+
+export default ItemsPageHeader;
