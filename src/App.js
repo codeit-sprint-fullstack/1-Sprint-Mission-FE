b0bece78e3b2d0c9366272537aea062ec26fa44f
minseokim <claudia503@naver.com>
2024-08-07 21:09:32 +0900

    mission6 Initial commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import ProductList from './components/ProductList';
+import Registration from './components/Registration';
 import './App.css';
 
 const App = () => {
   return (
     <div className="App">
-      <Navbar />
-      <ProductList />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<ProductList />} />
+          <Route path="/registration" element={<Registration />} />
+        </Routes>
+      </Router>
     </div>
   );
 };
