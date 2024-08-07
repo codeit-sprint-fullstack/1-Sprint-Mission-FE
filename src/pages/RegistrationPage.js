dd1c3878349de2d4bfa8325f92b2656b2e6ff657
S2OHEE <s2h110@naver.com>
2024-08-07 14:28:35 +0900

    feat: 입력 필드 변경 시 상태 업데이트 핸들러 추가

--- src/pages/RegistrationPage.js
+++ src/pages/RegistrationPage.js
@@ -13,6 +13,14 @@ function RegistrationPage() {
   const [values, setValues] = useState(INITIAL_VALUES);
   const [tags, setTags] = useState([]);
 
+  // 입력 필드 변경시, 상태 업데이트 핸들러
+  const handleInputChange = (name, value) => {
+    setValues((prevValues) => ({
+      ...prevValues,
+      [name]: value,
+    }));
+  };
+
   return (
     <div className='RegistrationPage'>
       <ItemsPageHeader />
@@ -29,6 +37,8 @@ function RegistrationPage() {
               className={`RegistrationInput`}
               type="text"
               name="name"
+              value={values.name}
+              onChange={handleInputChange}
               placeholder='상품명을 입력해주세요'
               required
             />
@@ -39,6 +49,8 @@ function RegistrationPage() {
               id='Input2'
               name="description"
               className={`RegistrationInput`}
+              value={values.description}
+              onChange={handleInputChange}
               placeholder='상품 소개를 입력해주세요'
               required
             />
@@ -50,6 +62,8 @@ function RegistrationPage() {
               className={`RegistrationInput`}
               type="number"
               name="price"
+              value={values.price}
+              onChange={handleInputChange}
               placeholder='판매 가격을 입력해주세요'
               required
             />
@@ -61,6 +75,8 @@ function RegistrationPage() {
               className={`RegistrationInput`}
               type="text"
               name="tags"
+              value={values.tags}
+              onChange={handleInputChange}
               placeholder='#태그 형식으로 입력해주세요 (예시, #모자)'
               required
             />
