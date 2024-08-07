7187eb90598a17c3b7765d1850bcce74c8c3104d
S2OHEE <s2h110@naver.com>
2024-08-07 15:30:53 +0900

    상품 상세페이지 ProductInformationPage 경로 추가

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom'; 
 import ItemsPage from './pages/ItemsPage';
 import RegistrationPage from './pages/RegistrationPage'; 
+import ProductinformationPage from './pages/ProductinformationPage'; // 상품 등록버튼 클릭시 이동하는 빈페이지
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/items" element={<ItemsPage />}></Route>
         <Route path="/registration" element={<RegistrationPage />}></Route>
+        <Route path="/Productinformation" element={<ProductinformationPage />}></Route>
       </Routes>
     </div>
   );
