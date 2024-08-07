3be74b29ee1ac4fed7af2509985f2f122f84c39d
minsu <a2238737@gmail.com>
2024-08-07 14:31:22 +0900

    docs: 이름변경

--- src/header.js
+++ src/header.js
@@ -1,23 +1,22 @@
-import Header_logo from './img/Header_logo.svg'
-import Header_my from './img/Header_my.svg'
-import './header.css'
+import HeaderLogo from "./img/Header_logo.svg";
+import HeaderMy from "./img/Header_my.svg";
+import "./header.css";
 
-function Headers(){
-  return(
+function Header() {
+  return (
     <div id="header">
       <a href="https://extraordinary-lily-d8e584.netlify.app/">
-        <img id="header_logo_img" alt="" src={Header_logo}></img>
+        <img id="header-logo-img" alt="" src={HeaderLogo}></img>
       </a>
-      
-      
-      <div id="header_str">
+
+      <div id="header-str">
         <p>자유게시판</p>
         <p>중고마켓</p>
       </div>
-      
-      <img id="header_my_img" alt="" src={Header_my}></img>
+
+      <img id="header-my-img" alt="" src={HeaderMy}></img>
     </div>
-  )
+  );
 }
 
-export default Headers
+export default Header;
