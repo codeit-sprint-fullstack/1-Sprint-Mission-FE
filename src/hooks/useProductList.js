53c51c7470a2a75f3fc103ec7de96f493b14a29c
S2OHEE <s2h110@naver.com>
2024-08-08 16:42:07 +0900

    fix:  Uncaught TypeError: products is not iterable 문제 해결을 위해 product가 배열인지 확인하는 코드 수정

--- src/hooks/useProductList.js
+++ src/hooks/useProductList.js
@@ -9,17 +9,26 @@ function useProductList(order, initialCursor) {
   const [totalPages, setTotalPages] = useState(1);
   const [cursor, setCursor] = useState(initialCursor);
 
-  const fetchProducts = useCallback(async (page) => {
+  const fetchProducts = useCallback(async () => {
     try {
       setLoadingError(null);
       const response = await getProductList({ order, cursor, limit: LIMIT });
       const { paging, list, totalCount } = response;
 
-      setProducts(list); // 전체 상품 목록 설정
+      // 응답 데이터의 구조 확인
+      console.log('받아온 객체:',response);
+      console.log('list:', response.list);
+      console.log('paging:', response.paging);
+      console.log('totalCount:', response.totalCount);
 
-      setCursor(paging ? paging.nextCursor : null);
-      setHasNext(paging ? paging.hasNext : false);
-      setTotalPages(totalCount ? Math.ceil(totalCount / LIMIT) : 5);
+      if (Array.isArray(response)) {
+        setProducts(response); // 전체 상품 목록 설정
+        setCursor(paging ? paging.nextCursor : null);
+        setHasNext(paging ? paging.hasNext : false);
+        setTotalPages(totalCount ? Math.ceil(totalCount / LIMIT) : 5);
+      } else {
+        throw new Error('상품 목록 데이터가 유효하지 않습니다.');
+      }
     } catch (error) {
       setLoadingError(error.message);
     }
