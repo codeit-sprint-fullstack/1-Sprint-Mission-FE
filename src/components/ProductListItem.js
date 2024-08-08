8094b03f6c0a34e2dd1aba479747fee6f22b27c5
S2OHEE <s2h110@naver.com>
2024-08-08 15:39:53 +0900

    상품 이미지 없는 경우, 기본 이미지 사용하도록 코드 추가

--- src/components/ProductListItem.js
+++ src/components/ProductListItem.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './ProductListItem.css';
+import img_default from '../assets/images/img_default.png';
 
 function ProductListItem({ product }) {
+
+  // 상품 이미지 없는 경우, 기본이미지 사용
+  const productImage = product.images && product.images.length > 0 ? product.images[0] : img_default;
+  
   return (
     <div className="product-card">
-      <img src={product.images[0]} alt={product.name} className="product-image" />
+      <img src={productImage} alt={product.name} className="product-image" />
       <div className="product-info">
         <p className="product-name">{product.name}</p>
         <p className="product-price">{product.price}원</p>
