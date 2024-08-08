322b3a3c881ab2a0da7afa7d2ddd7244a24e37f2
S2OHEE <s2h110@naver.com>
2024-08-08 17:05:56 +0900

    임시 메인페이지 추가(추후 수정 예정)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom'; 
+import MainPage from './pages/MainPage';
 import ItemsPage from './pages/ItemsPage';
 import RegistrationPage from './pages/RegistrationPage'; 
 import ProductInformationPage from './pages/ProductInformationPage'; // 상품 등록버튼 클릭시 이동하는 빈페이지
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <Routes>
+        <Route path="/" element={<MainPage />}></Route>
         <Route path="/items" element={<ItemsPage />}></Route>
         <Route path="/registration" element={<RegistrationPage />}></Route>
         <Route path="/Productinformation" element={<ProductInformationPage />}></Route>
