18205985c9e946ccd521a2b6c12e91a6cabc9805
jongmin <charlieko123@github.com>
2024-08-08 09:38:56 +0900

    add postItem

--- src/api.js
+++ src/api.js
@@ -16,3 +16,22 @@ export const getItems = async (page = 1, pageSize = 10, keyword = "") => {
     return { totalProducts: 0, products: [] };
   }
 };
+
+export const postItem = async (productData) => {
+  try {
+    const res = await fetch(URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(productData),
+    });
+    if (!res.ok) {
+      throw new Error(res.status);
+    }
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
