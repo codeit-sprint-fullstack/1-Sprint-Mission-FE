07ed0209af39a459c58b2429e087af744f81d2cc
minseokim <claudia503@naver.com>
2024-08-09 01:00:36 +0900

    feat: mission6_ProductCard.js ProductCard 컴포넌트에 중고마켓 페이지 조건 추가

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './ProductCard.css';
+import { useLocation } from 'react-router-dom';
 
 const ProductCard = ({ product, isBestProduct }) => {
+  const location = useLocation();
+  const isMarketPage = location.pathname === '/items';
+  const isDefaultImage = isMarketPage && (!product.images || product.images.length === 0);
+  
   return (
     <div className={`product-card ${isBestProduct ? 'best-product-card' : ''}`}>
-      <img src={product.images[0]} alt={product.name} className={`product-image ${isBestProduct ? 'best-product-image' : ''}`} />
+      <img 
+        src={isDefaultImage ? 'image/img_default.svg' : product.images[0]} 
+        alt={product.name} 
+        className={`product-image ${isBestProduct ? 'best-product-image' : ''} ${isDefaultImage ? 'default-size' : ''}`}
+      />
       <h3>{product.name}</h3>
       <p className="product-price">{product.price.toLocaleString()}원</p>
       <div className="favorite">
