b82c4bc37abdbddfdd8cf6e711009879326518a5
amberim <im.amberim@gmail.com>
2024-08-09 14:25:28 +0900

    style: applied basic form styles, feature: added logic to add tag in tag input.

--- src/components/RegistrationForm/RegistrationForm.jsx
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -2,24 +2,52 @@ import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 import { postProduct } from '../../services/api';
 
+import './RegistrationForm.css';
+
 export default function RegistrationForm({ className, initialValue }) {
-  const [values, setValues] = useState(initialValue);
+  const [inputValues, setInputValues] = useState(initialValue);
+  const [tagInputValue, setTagInputValue] = useState('');
+  const [tags, setTags] = useState([]);
   const navigate = useNavigate();
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
-    setValues((prevInputValues) => ({
+    setInputValues((prevInputValues) => ({
       ...prevInputValues,
       [name]: value,
     }));
   };
 
+  const handleTagInputChange = (e) => {
+    const value = e.target.value;
+    setTagInputValue(value);
+  };
+
+  const addTag = () => {
+    const newTag = tagInputValue.trim();
+    if (newTag !== '') {
+      if (!tags.includes(newTag)) {
+        setTags((prevTags) => [...prevTags, newTag]);
+        setTagInputValue('');
+      } else {
+        console.log('이미 입력한 태그임');
+      }
+    }
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter' && !e.nativeEvent.isComposing) {
+      e.preventDefault();
+      addTag();
+    }
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     const formData = new FormData();
-    formData.append('name', values.name);
-    formData.append('description', values.description);
-    formData.append('price', values.price);
+    formData.append('name', inputValues.name);
+    formData.append('description', inputValues.description);
+    formData.append('price', inputValues.price);
 
     try {
       const response = await postProduct(formData);
@@ -37,46 +65,54 @@ export default function RegistrationForm({ className, initialValue }) {
 
   return (
     <form onSubmit={handleSubmit} className={className}>
-      <div>
+      <div className='form-top'>
         <h2>상품 등록하기</h2>
         <button type='submit'>등록</button>
       </div>
-      <label htmlFor='name'>상품평</label>
-      <input
-        id='name'
-        name='name'
-        type='text'
-        value={values.name}
-        onChange={handleInputChange}
-        placeholder='상품평을 입력해주세요'
-      />
-      <label htmlFor='description'>상품 소개</label>
-      <textarea
-        id='description'
-        name='description'
-        type='text'
-        value={values.description}
-        onChange={handleInputChange}
-        placeholder='상품 소개를 입력해 주세요'
-      />
-      <label htmlFor='price'>판매가격</label>
-      <input
-        id='price'
-        name='price'
-        type='number'
-        value={values.price}
-        onChange={handleInputChange}
-        placeholder='판매가격을 입력해주세요'
-      />
-      <label htmlFor='tags'>태그</label>
-      <input
-        id='tags'
-        name='tags'
-        type='text'
-        value={values.tags}
-        onChange={handleInputChange}
-        placeholder='태그를 입력해주세요'
-      />
+      <div className='form-inputs'>
+        <label htmlFor='name'>상품평</label>
+        <input
+          id='name'
+          name='name'
+          type='text'
+          value={inputValues.name}
+          onChange={handleInputChange}
+          placeholder='상품평을 입력해주세요'
+        />
+        <label htmlFor='description'>상품 소개</label>
+        <textarea
+          id='description'
+          name='description'
+          type='text'
+          value={inputValues.description}
+          onChange={handleInputChange}
+          placeholder='상품 소개를 입력해 주세요'
+        />
+        <label htmlFor='price'>판매가격</label>
+        <input
+          id='price'
+          name='price'
+          type='number'
+          value={inputValues.price}
+          onChange={handleInputChange}
+          placeholder='판매가격을 입력해주세요'
+        />
+        <label htmlFor='tags'>태그</label>
+        <input
+          id='tags'
+          name='tags'
+          type='text'
+          value={tagInputValue}
+          onChange={handleTagInputChange}
+          onKeyDown={handleKeyDown}
+          placeholder='태그를 입력해주세요'
+        />
+        <ul>
+          {tags.map((tag, index) => {
+            return <li key={index}>{tag}</li>;
+          })}
+        </ul>
+      </div>
     </form>
   );
 }
