9a25e7c0acb0467fdd02c4053cdf25e77bfe89e3
minseokim <claudia503@naver.com>
2024-08-10 23:56:27 +0900

    feat: mission6_ProductCard.js 상품 이미지가 null일때 발생하는 오류를 방지하기 위해, 모든 상품이미지를 기본이미지로 설정

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -5,14 +5,15 @@ import { useLocation } from 'react-router-dom';
 const ProductCard = ({ product, isBestProduct }) => {
   const location = useLocation();
   const isMarketPage = location.pathname === '/items';
-  const isDefaultImage = isMarketPage && (!product.images || product.images.length === 0);
-  
+
+  const imageSrc = '/image/img_default.svg';
+
   return (
     <div className={`product-card ${isBestProduct ? 'best-product-card' : ''}`}>
       <img 
-        src={isDefaultImage ? 'image/img_default.svg' : product.images[0]} 
+        src={imageSrc} 
         alt={product.name} 
-        className={`product-image ${isBestProduct ? 'best-product-image' : ''} ${isDefaultImage ? 'default-size' : ''}`}
+        className={`product-image ${isBestProduct ? 'best-product-image' : ''}`}
       />
       <h3>{product.name}</h3>
       <p className="product-price">{product.price.toLocaleString()}원</p>
