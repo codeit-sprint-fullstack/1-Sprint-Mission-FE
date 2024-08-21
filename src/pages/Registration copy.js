39e4c664d764e068d24c850dcca28dc96f445f49
Jun5 <chdh1005@gmail.com>
2024-08-21 15:25:30 +0900

    [Chore] 상품 등록 페이지 내 사용 컴포넌트 명 & CSS 모듈화 수정

---
+++ src/pages/Registration copy.js
@@ -0,0 +1,104 @@
+import React from "react";
+import { useState } from "react";
+import styles from "./Registration.module.css";
+
+//렌더링 컴포넌트
+import ProductHeaderText from "../components/common/ProductHeaderText.js";
+import ProductDataRegistBtn from "../components/ProductDataRegistBtn.js";
+
+import RegistrationInput from "../components/common/RegistrationInput.js";
+import RegistrationTextArea from "../components/common/textArea.js";
+
+//렌더링 프레임
+import PageNavRender from "../components/PageNavRender.js";
+
+// 커스텀 훅
+import useValidationText from "../hooks/useValidationText.js";
+
+function Registration() {
+  const [inputDiscriptionText, setInputDiscriptionText] = useState("");
+  const [inputNameText, setinpuNametText] = useState("");
+  const [inputPrice, setInputPrice] = useState("");
+  const [inputTagText, setInputTagText] = useState("");
+
+  const {
+    nameValidation,
+    priceValidation,
+    discriptionValidation,
+    tagValidation,
+    validationForm
+  } = useValidationText(
+    inputNameText,
+    inputPrice,
+    inputDiscriptionText,
+    inputTagText
+  );
+
+  const handleinpuNametText = (e) => {
+    setinpuNametText(e.target.value);
+  };
+
+  const handleInputDiscriptionText = (e) => {
+    setInputDiscriptionText(e.target.value);
+  };
+
+  const handleInputPrice = (e) => {
+    setInputPrice(e.target.value);
+  };
+
+  const handleInputTagText = (e) => {
+    setInputTagText(e.target.value);
+  };
+
+  return (
+    <div className="registrationPage">
+      <nav>
+        <PageNavRender marketBoardActive={true} loginStatus={false}/>
+      </nav>
+
+      <main>
+        <header className="registrationHeader">
+          <ProductHeaderText headerText={"상품 등록하기"} />
+          <ProductDataRegistBtn registerBtnActive={validationForm} />
+        </header>
+        <section className="mainInputSectionSet">
+          <RegistrationInput
+            headerText={"상품명"}
+            inputText={inputNameText}
+            handleinputText={handleinpuNametText}
+            placeholderText={"상품명를 입력해주세요"}
+            validationActive={nameValidation}
+            validationMessage={"10자 이내로 입력해주세요"}
+          />
+          <RegistrationTextArea
+            headerText={"상품 소개"}
+            inputText={inputDiscriptionText}
+            handleinputText={handleInputDiscriptionText}
+            placeholderText={"상품 소개를 입력해주세요"}
+            validationActive={discriptionValidation}
+            validationMessage={"10자 이상 입력해주세요"}
+          />
+          <RegistrationInput
+            headerText={"판매가격"}
+            inputText={inputPrice}
+            handleinputText={handleInputPrice}
+            placeholderText={"판매 가격을 입력해주세요"}
+            validationActive={priceValidation}
+            validationMessage={"숫자로 입력해주세요"}
+          />
+          <RegistrationInput
+            headerText={"태그"}
+            inputText={inputTagText}
+            handleinputText={handleInputTagText}
+            placeholderText={"태그를 입력해주세요"}
+            validationActive={tagValidation}
+            validationMessage={"5글자 이내로 입력해주세요"}
+          />
+        </section>
+      </main>
+
+    </div>
+  );
+}
+
+export default Registration;
