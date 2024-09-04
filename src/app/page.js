452bcec70cc1175c4f03d4f9f95c66c600377db6
S2OHEE <s2h110@naver.com>
2024-09-04 11:25:15 +0900

    chore: 메인페이지를 src/app/page.js로 이동

---
+++ src/app/page.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../components/Header";
+
+export default function MainPage() {
+  return (
+    <>
+      <Header />
+      <div>
+        <h1>메인 페이지(임시)</h1>
+      </div>
+    </>
+  );
+}
