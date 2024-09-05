2f89a6bf8d27f0a1f25ec072987f1a0c70fab3d2
S2OHEE <s2h110@naver.com>
2024-09-05 10:50:26 +0900

    Feat: 자유게시판 페이지 기본 구현

--- src/pages/free-board.js
+++ src/pages/free-board.js
@@ -1,14 +1,39 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import ItemsPageHeader from "../components/ItemsPageHeader";
+import styles from "./FreeBoardPage.module.css";
 
 export default function FreeBoardPage() {
   return (
-    <div className="freeBoardPage">
+    <div className={styles.freeBoardPage}>
       <ItemsPageHeader />
-      <div>
-        <h1>자유게시판</h1>
+      <div className={styles.bestPostsContainer}>
+        <h1>베스트 게시글</h1>
+        <div className={styles.bestPosts}></div>
+        <div className={styles.postsContainer}>
+          <h2>게시글</h2>
+          <button className={styles.postBtn} onClick={handleAddPostClick}>
+            글쓰기
+          </button>
+        </div>
+        <div className={styles.searchBarDropdown}>
+          {/* 검색창 */}
+          <input
+            type="text"
+            placeholder="검색할 상품을 입력해주세요"
+            className={styles.searchInput}
+            style={{ backgroundImage: `url(/images/ic_search.png)` }} // public/images 폴더에서 이미지 접근
+            value={searchProduct}
+            onChange={handleSearchChange}
+            onKeyDown={handleKeyDown}
+          />
+          {/* 드롭 다운 */}
+          <select className={styles.sortDropDown} onChange={handleOrderChange}>
+            <option value="createdAt">최신순</option>
+            <option value="likes">좋아요순</option>
+          </select>
+        </div>
       </div>
     </div>
   );
