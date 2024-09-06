829957ff7f65ccfff0610f8e0a891cc6743ef3c6
Jun5 <chdh1005@gmail.com>
2024-09-06 13:47:05 +0900

    :wrench: [Chore] 리액트 기반 Link > Next Link로 바꾸기 위해 코드 삭제 > 추후에는 페이지에서 관리할 예정

--- src/components/common/BtnRoundFunction.js
+++ src/components/common/BtnRoundFunction.js
@@ -1,12 +1,10 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import styles from "./BtnRoundFunction.module.css";
 
 // 이미지
 
 function BtnRoundFunction({
   //기본 props
-  linkTo,
   innerText = "버튼",
   imgSrc,
 
@@ -19,31 +17,17 @@ function BtnRoundFunction({
   //상태 관리
   activeState = true,
 }) {
-
   return (
     <>
-      {linkTo ? (
-        <Link to={`/${linkTo}`}>
-          <button
-            className={activeState ? styles.activeBtn : styles.disabledBtn}
-            disabled={!activeState}
-            style={{ width: customWidth ? customWidth : '' }}
-            onClick={handleOnClick}
-          >
-            <span className={styles.btnText}>{innerText}</span>
-            {imgSrc && <img src={imgSrc} alt={"아이콘"} />}
-          </button>
-        </Link>
-      ) : (
-        <button
-          className={activeState ? styles.activeBtn : styles.disabledBtn}
-          disabled={!activeState}
-          style={{ width: customWidth ? customWidth : '' }}
-          onClick={handleOnClick}
-        >
-          {innerText}
-        </button>
-      )}
+      <button
+        className={activeState ? styles.activeBtn : styles.disabledBtn}
+        disabled={!activeState}
+        style={{ width: customWidth ? customWidth : "" }}
+        onClick={handleOnClick}
+      >
+        <span className={styles.btnText}>{innerText}</span>
+        {imgSrc && <img src={imgSrc} alt={"아이콘"} />}
+      </button>
     </>
   );
 }
--- src/components/common/BtnSquareFunction.js
+++ src/components/common/BtnSquareFunction.js
@@ -5,33 +5,19 @@ import styles from "./BtnSquareFunction.module.css";
 // 이미지
 
 function BtnSquareFunction({
-  linkTo,
   innerText = "버튼",
   handleOnClick = () => {},
   activeState = true,
 }) {
-  
   return (
     <>
-      {linkTo ? (
-        <Link to={`/${linkTo}`}>
-          <button
-            className={activeState ? styles.activeBtn : styles.disabledBtn}
-            disabled={!activeState}
-            onClick={handleOnClick}
-          >
-            {innerText}
-          </button>
-        </Link>
-      ) : (
-        <button
-          className={activeState ? styles.activeBtn : styles.disabledBtn}
-          disabled={!activeState}
-          onClick={handleOnClick}
-        >
-          {innerText}
-        </button>
-      )}
+      <button
+        className={activeState ? styles.activeBtn : styles.disabledBtn}
+        disabled={!activeState}
+        onClick={handleOnClick}
+      >
+        {innerText}
+      </button>
     </>
   );
 }
