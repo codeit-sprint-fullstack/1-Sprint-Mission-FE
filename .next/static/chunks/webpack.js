2a0533eeb970c089365e545415970b4d88c8232e
S2OHEE <s2h110@naver.com>
2024-09-07 23:50:03 +0900

    게시글 상세페이지 추가 및 헤더 import

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2ce38b31351e2e24"; }
+/******/ 		__webpack_require__.h = function() { return "55fccc983de887e4"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
---
+++ src/pages/post-detail.js
@@ -0,0 +1,9 @@
+import FreeBoardPageHeader from "../components/FreeBoardPageHeader"; // 자유게시판 헤더
+
+export default function PostDetailPage() {
+  return (
+    <div>
+      <FreeBoardPageHeader />
+    </div>
+  );
+}
