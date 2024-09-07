5af65adea2cdf392060c5337f2494d692dfdd4a5
S2OHEE <s2h110@naver.com>
2024-09-07 15:11:55 +0900

    Feat: 게시물 목록 조회 API 추가

--- src/api/api.js
+++ src/api/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 //const BASE_URL = 'https://panda-market-api.vercel.app/products';
 
 //로컬 개발 서버
-const BASE_URL = process.env.REACT_APP_API_URL;
+const BASE_URL = process.env.API_URL;
 console.log("BASE_URL:", BASE_URL);
 
 export async function getProductList({
@@ -47,14 +47,14 @@ export async function createProduct(product) {
         headers: {
           "Content-Type": "application/json",
         },
-      },
+      }
     );
     return response.data;
   } catch (error) {
     if (error.response) {
       console.error(
         "상품 생성 실패 - 서버에서 반환한 오류:",
-        error.response.data,
+        error.response.data
       );
     } else {
       console.error("상품 생성 실패 - 네트워크 오류:", error.message);
@@ -66,6 +66,18 @@ export async function createProduct(product) {
 /* 제품 목록을 필터링하는 함수 */
 export function filterProductsByName(products, query) {
   return products.filter((product) =>
-    product.name.toLowerCase().includes(query.toLowerCase()),
+    product.name.toLowerCase().includes(query.toLowerCase())
   );
 }
+
+/*---------------------게시글 관련 API 호출--------------------*/
+// 게시글 목록 조회
+export const fetchArticles = async (params = {}) => {
+  try {
+    const response = await axios.get(BASE_URL, { params });
+    return response.data;
+  } catch (error) {
+    console.error("게시글 목록 조회 실패:", error);
+    throw error;
+  }
+};
