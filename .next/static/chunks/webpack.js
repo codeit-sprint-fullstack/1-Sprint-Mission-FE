2d9dc7bdff04cfad0b06791851064ff39daa3408
S2OHEE <s2h110@naver.com>
2024-09-08 00:50:01 +0900

    게시글 상세페이지 수정 및 css 모듈 파일 추가

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "55fccc983de887e4"; }
+/******/ 		__webpack_require__.h = function() { return "14c74bfa960430a9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/pages/post-detail.js
+++ src/pages/post-detail.js
@@ -1,9 +1,14 @@
 import FreeBoardPageHeader from "../components/FreeBoardPageHeader"; // 자유게시판 헤더
+import PostDetail from "../components/PostDetail";
+import styles from "./PostDetailPage.module.css"; // css 모듈 파일 import
 
 export default function PostDetailPage() {
   return (
     <div>
       <FreeBoardPageHeader />
+      <main className={styles.main}>
+        <PostDetail />
+      </main>
     </div>
   );
 }
