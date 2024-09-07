39890f2e57a8851b3e1790dbd28751f0d1c4417b
S2OHEE <s2h110@naver.com>
2024-09-08 04:01:21 +0900

    Fix: 수정 및 삭제하기 메뉴 오류 해결

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a45bbdcc8cd95930"; }
+/******/ 		__webpack_require__.h = function() { return "0442c3579b161a20"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/components/FreeBoardCommentItem.js
+++ src/components/FreeBoardCommentItem.js
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import styles from "./FreeBoardCommentItem.module.css";
 import AuthorProfile from "../../public/images/profile-image.png";
+import UpdateDeleteButton from "../components/UpdateDeleteButton";
 
 export default function FreeBoardCommentItem({ author, content, date }) {
+  const [menuVisible, setMenuVisible] = useState(false);
+
+  const toggleMenu = () => setMenuVisible(!menuVisible);
+
   return (
     <div className={styles.commentContainer}>
       <div className={styles.commentHeader}>
         <p className={styles.content}>댓글 내용</p>
-        <button className={styles.moreMenuButton}>:</button>
+        <button className={styles.moreMenuButton} onClick={toggleMenu}>
+          :
+        </button>
       </div>
       <div className={styles.imageAuthorDate}>
         <Image
@@ -23,6 +30,9 @@ export default function FreeBoardCommentItem({ author, content, date }) {
           <span className={styles.date}>작성한 시간</span>
         </div>
       </div>
+
+      {/* 수정/삭제 메뉴 */}
+      {menuVisible && <UpdateDeleteButton />}
     </div>
   );
 }
