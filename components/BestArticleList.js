74ba8d1f717edd32fc9b1fbde8fd81777ec34834
mozzi34 <160555885+mozzi34@users.noreply.github.com>
2024-09-08 00:38:34 +0900

    add: CSS for Best Articles and Posts Viewing

---
+++ components/BestArticleList.js
@@ -0,0 +1,39 @@
+import Link from 'next/link';
+import articleImage from '@/public/article_image.png';
+import bestBadge from '@/public/best_badge.png';
+
+import Image from 'next/image';
+import styles from '@/styles/BestArticleList.module.css';
+import Date from '@/utils/Date.js';
+
+export default function BestArticleList({ articles = [] }) {
+  return (
+    <>
+      <div className={styles.mainText}>베스트 게시글</div>
+      <div className={styles.articleList}>
+        {articles.map((article) => (
+          <div key={article.id} className={styles.list}>
+            <Link href={`/article/${article.id}`} className={styles.link}>
+              <Image src={bestBadge} alt='베스트 뱃지' />
+              <div className={styles.main}>
+                <div className={styles.title}>{article.title}</div>
+                <Image
+                  src={articleImage}
+                  alt='기본이미지'
+                  className={styles.image}
+                />
+              </div>
+
+              <div className={styles.footer}>
+                <span className={styles.userName}>{article.user.name}</span>
+                <span className={styles.date}>
+                  <Date createDate={article} />
+                </span>
+              </div>
+            </Link>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
