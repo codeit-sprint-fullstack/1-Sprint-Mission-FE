6ba661dd901de172e49de0b11277cf93f1d0a1b5
mozzi34 <160555885+mozzi34@users.noreply.github.com>
2024-09-08 00:39:09 +0900

    refactor: navbar 링크 추가

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,6 +2,7 @@ import logo from '../public/logo.png';
 import loginBtn from '../public/login_btn.png';
 import Image from 'next/image';
 import styles from '@/styles/Navbar.module.css';
+import Link from 'next/link';
 
 export default function Navbar() {
   return (
@@ -9,8 +10,12 @@ export default function Navbar() {
       <div className={styles.nav}>
         <div className={styles.navMenu}>
           <div className={styles.menu}>
-            <Image src={logo} alt='로고' />
-            <span className={styles.freeboard}>자유게시판</span>
+            <Link href='/' className={styles.link}>
+              <Image src={logo} alt='로고' priority />
+            </Link>
+            <Link href='/fleamarket' className={styles.link}>
+              <span className={styles.freeboard}>자유게시판</span>
+            </Link>
             <span className={styles.fleamarket}>중고마켓</span>
           </div>
           <Image src={loginBtn} alt='로그인버튼' />
