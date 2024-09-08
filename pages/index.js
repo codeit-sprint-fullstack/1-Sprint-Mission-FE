777c4e99d245ef5a308d305c0521afcf175d39bc
mozzi34 <160555885+mozzi34@users.noreply.github.com>
2024-09-08 16:14:11 +0900

    add: 기본 페이지

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+import styles from '@/styles/Home.module.css';
+
+export default function Home() {
+  return (
+    <>
+      <div className={styles.body}></div>
+    </>
+  );
+}
