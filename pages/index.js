851114f11d8896414339e92cff5afad9702d1462
Jun5 <chdh1005@gmail.com>
2024-09-09 03:16:30 +0900

    :wrench: [Chore] Footer 반응형 구현

--- pages/index.js
+++ pages/index.js
@@ -16,57 +16,59 @@ import Footer from "@components/Footer";
 
 export default function RandingPage() {
   return (
-    <main className={styles.mainContainer}>
-      <TopBottomBanner>
-        <RenderOnlyCTA mainCTA={["일상의 모든 물건을 ", "거래해 보세요"]} />
-      </TopBottomBanner>
-      <ContentBanner>
-        <article className={styles.contentBox}>
-          <div className={styles.imgBox}>
-            <img src="/images/landing/home_01.png" alt="home_01" />
-          </div>
-          <CtaBox
-            keyword={"Hot item"}
-            mainCTA={["인기 상품을 ", "확인해 보세요"]}
-            description={[
-              "가장 HOT한 중고거래 물품을",
-              "판다 마켓에서 확인해 보세요",
-            ]}
-          />
-        </article>
-      </ContentBanner>
-      <ContentBanner>
-        <article className={styles.contentBox}>
-          <CtaBox
-            keyword={"Serch"}
-            mainCTA={["구매를 원하는", "상품을 검색하세요"]}
-            description={["구매하고 싶은 물품은 검색해서", "쉽게 찾아보세요"]}
-            alignRight={true}
-          />
-          <div className={styles.imgBox}>
-            <img src="/images/landing/home_02.png" alt="home_02" />
-          </div>
-        </article>
-      </ContentBanner>
-      <ContentBanner>
-        <article className={styles.contentBox}>
-          <div className={styles.imgBox}>
-            <img src="/images/landing/home_03.png" alt="home_03" />
-          </div>
-          <CtaBox
-            keyword={"Register"}
-            mainCTA={["판매를 원하는 ", "상품을 등록하세요"]}
-            description={[
-              "어떤 물건이든 판매하고 싶은 상품을",
-              "쉽게 등록하세요",
-            ]}
-          />
-        </article>
-      </ContentBanner>
-      <TopBottomBanner>
-        <CtaBox mainCTA={["믿을 수 있는", "판다마켓 중고 거래"]} />
-      </TopBottomBanner>
+    <>
+      <main className={styles.mainContainer}>
+        <TopBottomBanner>
+          <RenderOnlyCTA mainCTA={["일상의 모든 물건을 ", "거래해 보세요"]} />
+        </TopBottomBanner>
+        <ContentBanner>
+          <article className={styles.contentBox}>
+            <div className={styles.imgBox}>
+              <img src="/images/landing/home_01.png" alt="home_01" />
+            </div>
+            <CtaBox
+              keyword={"Hot item"}
+              mainCTA={["인기 상품을 ", "확인해 보세요"]}
+              description={[
+                "가장 HOT한 중고거래 물품을",
+                "판다 마켓에서 확인해 보세요",
+              ]}
+            />
+          </article>
+        </ContentBanner>
+        <ContentBanner>
+          <article className={styles.contentBox}>
+            <CtaBox
+              keyword={"Serch"}
+              mainCTA={["구매를 원하는", "상품을 검색하세요"]}
+              description={["구매하고 싶은 물품은 검색해서", "쉽게 찾아보세요"]}
+              alignRight={true}
+            />
+            <div className={styles.imgBox}>
+              <img src="/images/landing/home_02.png" alt="home_02" />
+            </div>
+          </article>
+        </ContentBanner>
+        <ContentBanner>
+          <article className={styles.contentBox}>
+            <div className={styles.imgBox}>
+              <img src="/images/landing/home_03.png" alt="home_03" />
+            </div>
+            <CtaBox
+              keyword={"Register"}
+              mainCTA={["판매를 원하는 ", "상품을 등록하세요"]}
+              description={[
+                "어떤 물건이든 판매하고 싶은 상품을",
+                "쉽게 등록하세요",
+              ]}
+            />
+          </article>
+        </ContentBanner>
+        <TopBottomBanner>
+          <CtaBox mainCTA={["믿을 수 있는", "판다마켓 중고 거래"]} />
+        </TopBottomBanner>
+      </main>
       <Footer />
-    </main>
+    </>
   );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,15 +10,17 @@ function Footer() {
         <div className={styles.copyright}>
           <p>©codeit - 2024</p>
         </div>
-        <div className={styles.contentMenu}>
-          <p>Privacy Policy</p>
-          <p>FAQ</p>
-        </div>
-        <div className={styles.iconSNS}>
-          <img src="/images/icon/ic_facebook.png" alt="facebook" />
-          <img src="/images/icon/ic_twitter.png" alt="twitter" />
-          <img src="/images/icon/ic_youtube.png" alt="youtube" />
-          <img src="/images/icon/ic_instagram.png" alt="instagram" />
+        <div className={styles.menuAndSNS}>
+          <div className={styles.contentMenu}>
+            <p>Privacy Policy</p>
+            <p>FAQ</p>
+          </div>
+          <div className={styles.iconSNS}>
+            <img src="/images/icon/ic_facebook.png" alt="facebook" />
+            <img src="/images/icon/ic_twitter.png" alt="twitter" />
+            <img src="/images/icon/ic_youtube.png" alt="youtube" />
+            <img src="/images/icon/ic_instagram.png" alt="instagram" />
+          </div>
         </div>
       </section>
     </footer>
