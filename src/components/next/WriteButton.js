17a6b4007cfe74c3bbdeb5d53f411bf3207c821e
minseokim <claudia503@naver.com>
2024-09-09 08:35:34 +0900

    feat: mission8_WrtieButton.js 글쓰기 버튼 컴포넌트 (Next.js 방식)

---
+++ src/components/next/WriteButton.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+import styles from './WriteButton.module.css';
+
+const WriteButton = () => {
+  const router = useRouter();
+
+  const handleClick = () => {
+    // 글쓰기 페이지로 이동
+    router.push('/articles/create');
+  };
+
+  return (
+    <button className={styles.writeButton} onClick={handleClick}>
+      글쓰기
+    </button>
+  );
+};
+
+export default WriteButton;
+
