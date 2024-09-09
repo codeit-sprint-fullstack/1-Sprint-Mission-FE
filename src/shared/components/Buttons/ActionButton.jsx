5a8d06fdd21d374b23574380f1e4557a14f60715
mini-1018 <anjaemin1018@gmail.com>
2024-09-09 16:17:04 +0900

    feat : 버튼 비활성화 및 비활성화 조건 추가

--- src/shared/components/Buttons/ActionButton.jsx
+++ src/shared/components/Buttons/ActionButton.jsx
@@ -3,22 +3,36 @@ import classNames from 'classnames';
 import styles from '@shared/components/Buttons/ActionButton.module.css';
 import { useRouter } from 'next/navigation';
 
-export default function ActionButton({ content, type, path }) {
+export default function ActionButton({ content, type, path, disabled }) {
   const router = useRouter();
 
   const buttonClass = classNames({
-    [styles['login-button']]: type === 'login',
-    [styles['signup-button']]: type === 'signup',
-    [styles['default-button']]: type === 'default',
-    [styles['write-button']]: type === 'write',
+    [styles['login-button']]: type === 'login', // 로그인
+    [styles['signup-button']]: type === 'signup', // 회원가입
+    [styles['post-button']]: type === 'post', // 게시글 등록
+    [styles['write-button']]: type === 'write', // 게시글 글쓰기
+    [styles['disabled']]: disabled,
   });
 
   const handleNavigate = (path) => {
     router.push(path);
   };
-  return (
-    <button className={buttonClass} onClick={() => handleNavigate(path)}>
-      {content}
-    </button>
-  );
+
+  const Button = () => {
+    if (path) {
+      return (
+        <button className={buttonClass} onClick={() => handleNavigate(path)}>
+          {content}
+        </button>
+      );
+    } else {
+      return (
+        <button className={buttonClass} disabled={disabled}>
+          {content}
+        </button>
+      );
+    }
+  };
+
+  return <Button />;
 }
