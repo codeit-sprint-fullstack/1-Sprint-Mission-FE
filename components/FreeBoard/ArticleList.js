e3d1af10fb9367ee7461901eac4b6d9be5011f42
mozzi34 <160555885+mozzi34@users.noreply.github.com>
2024-09-10 09:45:46 +0900

    refactor(mentor): ArticleList early return

--- components/FreeBoard/ArticleList.js
+++ components/FreeBoard/ArticleList.js
@@ -7,34 +7,38 @@ import DateFormat from '@/utils/DateFormat.js';
 import styles from '@/styles/ArticleList.module.css';
 
 export default function ArticleList({ articles = [] }) {
+  if (articles.length === 0) {
+    return (
+      <div className={styles.articleList}>
+        <span>둘러볼 게시글이 없습니다</span>
+      </div>
+    );
+  }
+
   return (
     <>
       <div className={styles.articleList}>
-        {articles.length > 0 ? (
-          articles.map((article) => (
-            <div key={article.id} className={styles.list}>
-              <Link href={`/article/${article.id}`} className={styles.link}>
-                <div className={styles.main}>
-                  <span className={styles.title}>{article.title}</span>
-                  <Image src={articleImage} alt='기본이미지' />
-                </div>
+        {articles.map((article) => (
+          <div key={article.id} className={styles.list}>
+            <Link href={`/article/${article.id}`} className={styles.link}>
+              <div className={styles.main}>
+                <span className={styles.title}>{article.title}</span>
+                <Image src={articleImage} alt='기본이미지' />
+              </div>
 
-                <div className={styles.footer}>
-                  <div>
-                    <Image src={profileIcon} alt='프로필 이미지' />
-                    <span className={styles.userName}>{article.user.name}</span>
-                    <span className={styles.date}>
-                      <DateFormat createDate={article} />
-                    </span>
-                  </div>
-                  <Image src={heartIcon} alt='하트 아이콘' />
+              <div className={styles.footer}>
+                <div>
+                  <Image src={profileIcon} alt='프로필 이미지' />
+                  <span className={styles.userName}>{article.user.name}</span>
+                  <span className={styles.date}>
+                    <DateFormat createDate={article} />
+                  </span>
                 </div>
-              </Link>
-            </div>
-          ))
-        ) : (
-          <span>둘러볼 게시글이 없습니다</span>
-        )}
+                <Image src={heartIcon} alt='하트 아이콘' />
+              </div>
+            </Link>
+          </div>
+        ))}
       </div>
     </>
   );
