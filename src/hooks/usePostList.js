95bfac56f7f8cfdf2044df88af8369ec915d0761
S2OHEE <s2h110@naver.com>
2024-09-10 15:43:27 +0900

    usePostList 훅이 데이터를 페이지별로 가져오고, 더 이상 데이터가 없는 경우를 처리하도록 수정

--- src/hooks/usePostList.js
+++ src/hooks/usePostList.js
@@ -2,34 +2,29 @@ import { useState, useEffect, useCallback } from "react";
 import { fetchArticles } from "../api/api"; // 게시글 API 호출 함수
 import { LIMIT } from "../constants"; // 상수
 
-export default function usePostList(order, initialCursor) {
+export default function usePostList(order) {
   const [posts, setPosts] = useState([]);
-  const [hasNext, setHasNext] = useState(false);
+  const [hasNext, setHasNext] = useState(true); // 초기값을 true로 설정
   const [loadingError, setLoadingError] = useState(null);
-  const [totalPages, setTotalPages] = useState(1);
-  const [cursor, setCursor] = useState(initialCursor);
-  const [loading, setLoading] = useState(true);
+  const [cursor, setCursor] = useState(null); // 초기 커서를 null로 설정
+  const [loading, setLoading] = useState(false); // 로딩 상태 기본값을 false로 설정
 
   // 게시글 가져오기 함수
   const fetchPosts = useCallback(
-    async (page = 1) => {
+    async (append = false) => {
       try {
         setLoading(true);
         setLoadingError(null);
         const response = await fetchArticles({ order, cursor, limit: LIMIT });
         console.log("API 응답:", response);
 
-        // 직접 배열을 응답으로 사용
         const { paging, totalCount } = response;
         const list = response; // API 응답의 최상위 요소가 리스트
 
         if (Array.isArray(list)) {
-          setPosts((prevPosts) =>
-            page === 1 ? list : [...prevPosts, ...list]
-          );
+          setPosts((prevPosts) => (append ? [...prevPosts, ...list] : list));
           setCursor(paging ? paging.nextCursor : null);
           setHasNext(paging ? paging.hasNext : false);
-          setTotalPages(totalCount ? Math.ceil(totalCount / LIMIT) : 1);
         } else {
           throw new Error("게시글 목록 데이터가 유효하지 않습니다.");
         }
@@ -46,5 +41,5 @@ export default function usePostList(order, initialCursor) {
     fetchPosts(); // 초기 로드
   }, [order, fetchPosts]);
 
-  return { posts, hasNext, loadingError, totalPages, fetchPosts, loading };
+  return { posts, hasNext, loadingError, fetchPosts, loading };
 }
