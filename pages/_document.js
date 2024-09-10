61bd410a055aaee86867e2e961d43d1160425a1a
minseokim <claudia503@naver.com>
2024-09-10 09:26:16 +0900

    fix: mission8 _document.js 글꼴 제거

--- pages/_document.js
+++ pages/_document.js
@@ -4,8 +4,7 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        {/* 메타 데이터 또는 폰트 로딩 등을 설정할 수 있습니다 */}
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Pretendard:wght@400;600&display=swap" />
+   
       </Head>
       <body>
         <Main />
