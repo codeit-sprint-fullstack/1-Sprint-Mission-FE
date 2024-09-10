77c724b5db82a9ee3d0605829c83f628be4ebc2d
Jun5 <chdh1005@gmail.com>
2024-09-10 18:15:11 +0900

    :recycle: [Refactor] ProductList 렌더링 컴포넌트 CSS 정리 및 Null값 예외처리 & 태그 정리

--- src/components/renderData/ProductList.js
+++ src/components/renderData/ProductList.js
@@ -6,37 +6,46 @@ const iconHeart = "/images/icon/ic_heart.svg";
 
 function ProductList({ dataList }) {
   const ProductRender = ({ dataObject }) => {
-    const { images, name, description, price, favoriteCount } = product;
-    try {
-      const [img] = images;
-    } catch (error) {
-      const img = product.images
-    }
-    
+    if (dataObject) {
+      const { images, name, description, price, favoriteCount } = dataObject;
+      const img= images[0];
 
-    return (
-      <li className={styles.ProductBox}>
-        <article className={styles.imgSizeControl}>
-          <img src={img} alt={name} />
-        </article>
-        <figure className={styles.ProductDescription}>
-          <span>{description.toLocaleString()}</span>
-          <h1>{`${price.toLocaleString()}원`}</h1>
-          <div className={styles.ProductLike}>
-            <img src={iconHeart} alt="좋아요" />
-            <span>{favoriteCount}</span>
+
+      return (
+        <figure className={styles.ProductBox}>
+          <div className={styles.imgSizeControl}>
+            <img src={img} alt={name} />
           </div>
+          <figcaption className={styles.ProductInfoBox}>
+            <p className={styles.description}>{description}</p>
+            <p className={styles.price}>{`${price.toLocaleString()}원`}</p>
+            <section className={styles.likeBox}>
+              <img src={iconHeart} alt="좋아요" />
+              <p>{favoriteCount}</p>
+            </section>
+          </figcaption>
         </figure>
-      </li>
-    );
+      );
+    } else {
+      return (
+        <figure className={styles.ProductBox}>
+          <div className={styles.imgSizeControl}>
+            <img src="/images/default/default_product.png" alt="상품 준비 중" />
+          </div>
+          <figcaption className={styles.productReady}>
+            <p >상품 준비 중</p>
+          </figcaption>
+        </figure>
+      );
+    }
   };
 
   return (
-    <ul className={styles.productContainer}>
+    <article className={styles.productContainer}>
       {dataList.map((item, idx) => (
         <ProductRender key={item.id ? item.id : idx - 30} dataObject={item} />
       ))}
-    </ul>
+    </article>
   );
 }
 
