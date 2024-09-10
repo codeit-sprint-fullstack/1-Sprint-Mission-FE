791e18b34a3a611192af2ec4718a1d28e5ded908
sanho <dieuaimer@gmail.com>
2024-09-10 22:37:26 +0900

    chore: Change page component name

--- components/Button.js
+++ components/Button.js
@@ -2,8 +2,7 @@ import styles from '@/components/Button.module.css';
 
 // Button component
 // 각 페이지에서 사용되는 버튼
-// prop으로 name과 size를 내려받는다.
-// size는 inline style로 패딩에 적용된다. 값이 존재할 경우 버튼 크기를 변경한다.
+// prop으로 name을 내려받는다.
 
 export default function Button({ className = '', name, ...props }) {
   const classNames = `${styles.button} ${className}`;
--- pages/postWriting.js
+++ pages/postWriting.js
@@ -3,7 +3,7 @@ import axios from '@/lib/axios';
 import { useRouter } from 'next/router';
 import { useState } from 'react';
 
-export default function postWriting() {
+export default function PostWriting() {
   // CreatePostForm에 title과 content 상태, 그리고 상태 변경 함수 전달
   // CreatePostButton에 isFormValid 전달해서 버튼 설정(사용자 입력 부분이 비워져 있으면 disabled)
   // 등록버튼(CreatePostButton) 눌렀을 때 API 요청 후 해당 게시물 상세 페이지로 이동
