48f3ca06c5f4bdfde1c5692bd54776a5d3472064
minseokim <claudia503@naver.com>
2024-09-11 14:29:20 +0900

    feat: mission8_PostItem.js 게시글을 클릭했을 때 해당 게시글의 상세 페이지로 이동

--- src/components/next/PostItem.js
+++ src/components/next/PostItem.js
@@ -1,33 +1,35 @@
-import Link from 'next/link';
+import React from 'react';
+import { useRouter } from 'next/router';
 import styles from './PostItem.module.css';
 
-const PostItem = ({ id, title, author, date, likes, image, isDefault = false }) => {
-  const randomNickname = '익명의 사용자';  
-  const randomLikes = Math.floor(Math.random() * 10000); 
+const PostItem = ({ id, title, author, date, likes, image }) => {
+  const router = useRouter(); // useRouter 훅 초기화
 
-  const postLink = isDefault ? '/post-detail' : `/articles/${id}`;
+  // 게시글 클릭 시 호출되는 함수
+  const handleClick = () => {
+    router.push(`/articles/${id}`); // 해당 게시글의 id를 기반으로 상세 페이지로 이동
+  };
+
+  const randomNickname = '익명의 사용자';
+  const randomLikes = Math.floor(Math.random() * 10000);
 
   return (
-    <div className={styles.postItem}>
-      <Link href={postLink}>
-        <a>
-          <div className={styles.postContent}>
-            <h3 className={styles.postTitle}>{title || '기본 제목'}</h3>
-            <div className={styles.postDetails}>
-              <img src="/image/mini_profile.svg" alt="Profile Icon" className={styles.profileIcon} />
-              <span className={styles.author}>{author || randomNickname}</span>
-              <span className={styles.date}>{date || '2024.04.16'}</span>
-            </div>
-          </div>
-          <div className={styles.postImageContainer}>
-            <img src={image || '/image/img_default.svg'} alt="Post Image" className={styles.postImage} />
-            <div className={styles.postLikes}>
-              <img src="/image/heart.svg" alt="Likes" />
-              <span>{likes !== undefined ? likes : randomLikes}</span>
-            </div>
-          </div>
-        </a>
-      </Link>
+    <div className={styles.postItem} onClick={handleClick}> {/* 클릭 시 handleClick 호출 */}
+      <div className={styles.postContent}>
+        <h3 className={styles.postTitle}>{title || '맥북 16인치 16기가 1테라 정도 사양이면 얼마에 팔아야하나요?'}</h3>
+        <div className={styles.postDetails}>
+          <img src="/image/profile.svg" alt="Profile Icon" className={styles.profileIcon} />
+          <span className={styles.author}>{author || randomNickname}</span> {/* author가 없으면 임의값 사용 */}
+          <span className={styles.date}>{date || '2024.04.16'}</span>
+        </div>
+      </div>
+      <div className={styles.postImageContainer}>
+        <img src={image || '/image/default.svg'} alt="게시글 이미지" className={styles.postImage} />
+        <div className={styles.postLikes}>
+          <img src="/image/heart.svg" alt="Likes" />
+          <span>{likes !== undefined ? likes : randomLikes}</span> {/* likes가 없으면 임의값 사용 */}
+        </div>
+      </div>
     </div>
   );
 };
