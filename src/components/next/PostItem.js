8ccfbf552ab0ab72843580f2490e83f0212457df
minseokim <claudia503@naver.com>
2024-09-11 14:35:40 +0900

    feat: mission8_PostItem.js 테스트용 기본값 객체 추가

--- src/components/next/PostItem.js
+++ src/components/next/PostItem.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useRouter } from 'next/router';
 import styles from './PostItem.module.css';
 
-const PostItem = ({ id, title, author, date, likes, image }) => {
+const PostItem = ({ id = 0, title, author, date, likes, image }) => {
   const router = useRouter(); // useRouter 훅 초기화
 
   // 게시글 클릭 시 호출되는 함수
@@ -10,24 +10,30 @@ const PostItem = ({ id, title, author, date, likes, image }) => {
     router.push(`/articles/${id}`); // 해당 게시글의 id를 기반으로 상세 페이지로 이동
   };
 
-  const randomNickname = '익명의 사용자';
-  const randomLikes = Math.floor(Math.random() * 10000);
+  // 테스트용 기본값 객체 설정
+  const defaultData = {
+    title: '맥북 16인치 16기가 1테라 정도 사양이면 얼마에 팔아야하나요?',
+    author: '익명의 사용자',
+    date: '2024.04.16',
+    likes: Math.floor(Math.random() * 10000), 
+    image: '/image/default.svg'
+  };
 
   return (
     <div className={styles.postItem} onClick={handleClick}> {/* 클릭 시 handleClick 호출 */}
       <div className={styles.postContent}>
-        <h3 className={styles.postTitle}>{title || '맥북 16인치 16기가 1테라 정도 사양이면 얼마에 팔아야하나요?'}</h3>
+        <h3 className={styles.postTitle}>{title || defaultData.title}</h3> {/* 제목이 없을 경우 기본값 사용 */}
         <div className={styles.postDetails}>
           <img src="/image/profile.svg" alt="Profile Icon" className={styles.profileIcon} />
-          <span className={styles.author}>{author || randomNickname}</span> {/* author가 없으면 임의값 사용 */}
-          <span className={styles.date}>{date || '2024.04.16'}</span>
+          <span className={styles.author}>{author || defaultData.author}</span> {/* 작성자가 없을 경우 기본값 사용 */}
+          <span className={styles.date}>{date || defaultData.date}</span> {/* 날짜가 없을 경우 기본값 사용 */}
         </div>
       </div>
       <div className={styles.postImageContainer}>
-        <img src={image || '/image/default.svg'} alt="게시글 이미지" className={styles.postImage} />
+        <img src={image || defaultData.image} alt="게시글 이미지" className={styles.postImage} /> {/* 이미지가 없을 경우 기본값 사용 */}
         <div className={styles.postLikes}>
           <img src="/image/heart.svg" alt="Likes" />
-          <span>{likes !== undefined ? likes : randomLikes}</span> {/* likes가 없으면 임의값 사용 */}
+          <span>{likes !== undefined ? likes : defaultData.likes}</span> {/* 좋아요 수가 없을 경우 기본값 사용 */}
         </div>
       </div>
     </div>
