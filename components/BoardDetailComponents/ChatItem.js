247d8708ece05858a7201741732b59e0cc2cf68a
juyeongeun <80696592+juyeongeun@users.noreply.github.com>
2024-09-12 09:06:06 +0900

    refactor: timeAgo Util 분리

--- components/BoardDetailComponents/ChatItem.js
+++ components/BoardDetailComponents/ChatItem.js
@@ -5,28 +5,13 @@ import reply_empty from "@/images/img_reply_empty.png";
 import styles from "./ChatItem.module.css";
 import { useState } from "react";
 import { deleteComments } from "@/utils/chatApi";
+import { timeAgo } from "@/utils/timeAgo";
 
 export default function ChatItem({ comments, onEdit }) {
   const [openDropdownId, setOpenDropdownId] = useState(null);
 
   const toggleDropdown = (id) => {
     setOpenDropdownId(openDropdownId === id ? null : id);
-  };
-
-  const timeAgo = (date) => {
-    const now = new Date();
-    const createdAt = new Date(date);
-    const timeDiff = (now - createdAt) / 1000;
-
-    const seconds = Math.floor(timeDiff);
-    const minutes = Math.floor(timeDiff / 60);
-    const hours = Math.floor(timeDiff / 3600);
-    const days = Math.floor(timeDiff / (3600 * 24));
-
-    if (days > 0) return `${days}일 전`;
-    if (hours > 0) return `${hours}시간 전`;
-    if (minutes > 0) return `${minutes}분 전`;
-    return `${seconds + 1}초 전`;
   };
 
   const handleDelete = async (id) => {
