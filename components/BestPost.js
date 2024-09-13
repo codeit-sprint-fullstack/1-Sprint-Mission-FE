9010d50981f2dc1c66c98c07f763475e8ecef7fb
sanho <dieuaimer@gmail.com>
2024-09-13 09:21:46 +0900

    feat: Add navigation from freeboard post to detail page

--- components/BestPost.js
+++ components/BestPost.js
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import styles from './BestPost.module.css';
 import default_img from '@/public/default_img.svg';
 import bestBanner from '@/public/bestBanner.svg';
@@ -11,24 +12,28 @@ export default function BestPost({ posts = [] }) {
       <ul className={styles.bestContainer}>
         {posts.map((post) => (
           <div className={styles.bestListBox} key={post.id}>
-            <div className={styles.bestBanner}>
-              <Image src={bestBanner} alt="베스트 게시글 배너 이미지" />
-            </div>
-            <li>
-              <div className={styles.bestTop}>
-                <h4>{post.title}</h4>
-                <div className={styles.postImgWrapper}>
-                  <Image src={default_img} alt="기본 이미지" />
-                </div>
+            <Link href={`/posts/${post.id}`} className={styles.link}>
+              <div className={styles.bestBanner}>
+                <Image src={bestBanner} alt="베스트 게시글 배너 이미지" />
               </div>
-              <div className={styles.bestBottom}>
-                <div className={styles.bottomLeft}>
-                  <span className={styles.userName}>{post.username}</span>
-                  <span className={styles.like}>❤️9999+</span>
+              <li>
+                <div className={styles.bestTop}>
+                  <h4>{post.title}</h4>
+                  <div className={styles.postImgWrapper}>
+                    <Image src={default_img} alt="기본 이미지" />
+                  </div>
                 </div>
-                <div className={styles.bestFormatDate}>{formatDate(new Date(post.createdAt))}</div>
-              </div>
-            </li>
+                <div className={styles.bestBottom}>
+                  <div className={styles.bottomLeft}>
+                    <span className={styles.userName}>{post.username}</span>
+                    <span className={styles.like}>❤️9999+</span>
+                  </div>
+                  <div className={styles.bestFormatDate}>
+                    {formatDate(new Date(post.createdAt))}
+                  </div>
+                </div>
+              </li>
+            </Link>
           </div>
         ))}
       </ul>
--- components/PostList.js
+++ components/PostList.js
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import styles from './PostList.module.css';
 import default_img from '@/public/default_img.svg';
 import profile_img from '@/public/ic_profile.svg';
@@ -10,28 +11,30 @@ export default function PostList({ posts }) {
       <ul>
         {posts.map((post) => (
           <li key={post.id} className={styles.postList}>
-            <div className={styles.postContainer}>
-              <div className={styles.postTop}>
-                <h4 className={styles.postTitle}>{post.title}</h4>
-                <div className={styles.postImgWrapper}>
-                  <Image src={default_img} alt="기본 이미지" />
-                </div>
-              </div>
-              <div className={styles.postBottom}>
-                <div className={styles.bottomLeft}>
-                  <div className={styles.leftProfileImg}>
-                    <Image src={profile_img} alt="사용자 프로필" />
+            <Link href={`/posts/${post.id}`} className={styles.link}>
+              <div className={styles.postContainer}>
+                <div className={styles.postTop}>
+                  <h4 className={styles.postTitle}>{post.title}</h4>
+                  <div className={styles.postImgWrapper}>
+                    <Image src={default_img} alt="기본 이미지" />
                   </div>
-                  <span className={styles.userName}>{post.username}</span>
-                  <span className={styles.bestFormatDate}>
-                    {formatDate(new Date(post.createdAt))}
-                  </span>
                 </div>
-                <div className={styles.bottomRight}>
-                  <span className={styles.like}>❤️ 9999+</span>
+                <div className={styles.postBottom}>
+                  <div className={styles.bottomLeft}>
+                    <div className={styles.leftProfileImg}>
+                      <Image src={profile_img} alt="사용자 프로필" />
+                    </div>
+                    <span className={styles.userName}>{post.username}</span>
+                    <span className={styles.bestFormatDate}>
+                      {formatDate(new Date(post.createdAt))}
+                    </span>
+                  </div>
+                  <div className={styles.bottomRight}>
+                    <span className={styles.like}>❤️ 9999+</span>
+                  </div>
                 </div>
               </div>
-            </div>
+            </Link>
           </li>
         ))}
       </ul>
