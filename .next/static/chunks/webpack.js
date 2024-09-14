a3ef6b597a70d9f137539c03322a97e8c1951417
S2OHEE <s2h110@naver.com>
2024-09-14 17:50:54 +0900

    Refactor: 검색 관련 로직을 SearchBar 컴포넌트로 분리

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e34f3de1e6e68e97"; }
+/******/ 		__webpack_require__.h = function() { return "9d6e57946aeae77c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/pages/free-board.js
+++ src/pages/free-board.js
@@ -1,13 +1,12 @@
 import React, { useEffect, useState, useMemo, useCallback } from "react";
-import searchIcon from "../../public/images/ic_search.png";
 import FreeBoardPageHeader from "../components/FreeBoardPageHeader";
 import { useRouter } from "next/router";
 import PostList from "../components/PostList";
 import { filterPostsByName } from "../api/api";
 import BestPostsList from "../components/BestPostsList";
 import usePostList from "../hooks/usePostList";
-import { LIMIT } from "../constants";
 import Footer from "../components/Footer";
+import SearchBar from "../components/SearchBar";
 import styles from "./FreeBoardPage.module.css"; // CSS 모듈 import
 import Spinner from "../components/Spinner"; // Spinner import
 
@@ -69,25 +68,15 @@ export default function FreeBoardPage() {
     setSearchPosts(""); // 정렬 변경 시 검색 결과 초기화
   };
 
-  const handleKeyDown = (e) => {
-    if (e.key === "Enter") {
-      handleSearchClick();
-    }
-  };
-
-  const handleSearchChange = (event) => {
-    setSearchPosts(event.target.value);
-  };
-
-  const handleSearchClick = useCallback(() => {
-    if (searchPosts.trim() === "") {
+  const handleSearch = (searchQuery) => {
+    if (searchQuery.trim() === "") {
       setSearchResults([]);
       setSearchError("⚠ 검색어를 입력해 주세요.");
       return;
     }
 
     // 검색어에 맞는 게시글 필터링
-    const results = filterPostsByName(posts, searchPosts);
+    const results = filterPostsByName(posts, searchQuery);
 
     if (results.length === 0) {
       setSearchResults([]);
@@ -96,7 +85,7 @@ export default function FreeBoardPage() {
       setSearchResults(results);
       setSearchError(null);
     }
-  }, [searchPosts, posts]);
+  };
 
   const handleAddPostClick = () => {
     router.push("/post-registration");
@@ -131,21 +120,13 @@ export default function FreeBoardPage() {
             글쓰기
           </button>
         </div>
-        <div className={styles.inputDrop}>
-          <input
-            type="text"
-            placeholder="검색할 게시글을 입력해주세요"
-            className={styles.freeSearchInput}
-            style={{ backgroundImage: `url(${searchIcon.src})` }}
-            value={searchPosts}
-            onChange={handleSearchChange}
-            onKeyDown={handleKeyDown}
-          />
-          <select className={styles.sortDropDown} onChange={handleOrderChange}>
-            <option value="createdAt">최신순</option>
-            <option value="likeCount">좋아요 순</option>
-          </select>
-        </div>
+        <SearchBar
+          onSearch={handleSearch}
+          onOrderChange={handleOrderChange}
+          order={order}
+          searchPosts={searchPosts}
+          setSearchPosts={setSearchPosts}
+        />
         {loading && <Spinner />}
         {loadingError && !loading && (
           <div className={styles.searchError}>{loadingError}</div>
