878fbe6e2a917dd4ed320f4b084cd08223674a3c
KEN <recom54@gmail.com>
2024-09-17 21:31:43 +0900

    ♻️ [Refactor] 피드백 후 전면 수정 2

--- hooks/usePostMutation.js
+++ hooks/usePostMutation.js
@@ -1,8 +1,41 @@
 import { useMutation } from "react-query";
-import { postComment } from "@/utils/communityAPI";
+import {
+  postComment,
+  updatePost,
+  deletePost,
+  updateComment,
+  deleteComment,
+  createPost,
+} from "@/utils/communityAPI";
 
-const usePostMutation = (onSuccess, onError) => {
-  const mutation = useMutation(postComment, {
+const usePostMutation = (type, onSuccess, onError) => {
+  let mutationFn;
+
+  switch (type) {
+    case "createPost":
+      mutationFn = createPost;
+      break;
+    case "updatePost":
+      mutationFn = updatePost;
+      break;
+    case "deletePost":
+      mutationFn = deletePost;
+      break;
+    case "createComment":
+    case "postComment":
+      mutationFn = postComment;
+      break;
+    case "updateComment":
+      mutationFn = updateComment;
+      break;
+    case "deleteComment":
+      mutationFn = deleteComment;
+      break;
+    default:
+      throw new Error(`Invalid mutation type: ${type}`);
+  }
+
+  const mutation = useMutation(mutationFn, {
     onSuccess: (data) => {
       if (onSuccess) {
         onSuccess(data);
@@ -12,16 +45,12 @@ const usePostMutation = (onSuccess, onError) => {
       if (onError) {
         onError(error);
       }
-      console.error("댓글 등록 오류:", error);
+      console.error(`${type} 오류:`, error);
     },
   });
 
-  const mutateComment = ({ postId, content }) => {
-    mutation.mutate({ postId, content });
-  };
-
   return {
-    mutateComment,
+    mutate: mutation.mutate,
     isLoading: mutation.isLoading,
     isError: mutation.isError,
     error: mutation.error,
