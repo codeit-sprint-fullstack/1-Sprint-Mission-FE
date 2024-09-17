2830c583e769355182bceb2c7df75cd02fce9f81
mini-1018 <anjaemin1018@gmail.com>
2024-09-17 21:09:59 +0900

    feat : delete, patch api 추가

--- src/utils/api/api.js
+++ src/utils/api/api.js
@@ -28,3 +28,53 @@ export async function postArticle(params) {
     console.error(error.status);
   }
 }
+
+export async function deleteArticle(id) {
+  try {
+    const response = await api.delete(`/articles/${id}`);
+    return response;
+  } catch (error) {
+    console.error(error.status);
+  }
+}
+
+export async function patchArticle(id, params) {
+  try {
+    const response = await api.patch(`/articles/${id}`, params);
+    return response.data;
+  } catch (error) {
+    console.error(error.status);
+  }
+}
+
+export async function getComment(id) {
+  try {
+    const response = await api.get(`/articles/${id}/comments`);
+    return response.data;
+  } catch (error) {
+    console.error(error.response ? error.response.status : error.message);
+    return [];
+  }
+}
+
+export async function postComment({ articleId, content }) {
+  try {
+    const response = await api.post(`/articles/${articleId}/comments`, {
+      content,
+    });
+    return response.data;
+  } catch (error) {
+    console.error(error.status);
+  }
+}
+
+export async function deleteComment({ articleId, commentId }) {
+  try {
+    const response = await api.delete(
+      `/articles/${articleId}/comments/${commentId}`
+    );
+    return response;
+  } catch (error) {
+    console.error(error.status);
+  }
+}
