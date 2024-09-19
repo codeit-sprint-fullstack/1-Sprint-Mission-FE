f764757b3343ce0c61980b1a7b2040b3da67781e
amberim <im.amberim@gmail.com>
2024-09-19 13:35:19 +0900

    refactor: applied react-hook-form on SearchBar

--- components/form/SearchBar.jsx
+++ components/form/SearchBar.jsx
@@ -1,45 +1,31 @@
 import Image from "next/image";
 import styles from "./SearchBar.module.scss";
 import searchIcon from "../../public/assets/icons/ic_search.svg";
-import { useState } from "react";
-import Button from "../ui/Button";
+import { useFormContext } from "react-hook-form";
 
 export default function SearchBar({
   setKeyword,
   placeholder = "검색어를 입력해 주세요",
 }) {
-  const [search, setSearch] = useState("");
-
-  const handleValueChange = (e) => {
-    const value = e.target.value;
-    setSearch(value);
-  };
-  const handleClick = () => {
-    e.preventDefault();
-    setKeyword(search);
-  };
+  const { register } = useFormContext();
 
   const handleKeyDown = (e) => {
-    e.preventDefault();
     if (e.key === "Enter" && !e.nativeEvent.isComposing) {
-      setKeyword(search);
-      console.log(search);
+      e.preventDefault();
+      setKeyword(e.target.value);
     }
   };
 
   return (
     <form className={styles.SearchBar}>
       <input
+        {...register("articleKeyword")}
         placeholder={placeholder}
-        value={search}
-        name="search"
         onKeyDown={handleKeyDown}
-        onChange={handleValueChange}
-        autoComplete="off"
       />
-      <Button type="button" variant="icon" onClick={handleClick}>
+      <div className={styles.searchIcon}>
         <Image src={searchIcon} width={24} height={24} alt="search icon" />
-      </Button>
+      </div>
     </form>
   );
 }
