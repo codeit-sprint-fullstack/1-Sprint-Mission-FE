d51612e4ebbd0b4c11a6d5b32ae435b0becb4ec7
minseokim <claudia503@naver.com>
2024-09-22 20:31:52 +0900

    feat: mission9_useScreenType.js

--- src/hooks/useScreenType.js
+++ src/hooks/useScreenType.js
@@ -1,32 +1,40 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 const getScreenType = () => {
-  const width = window.innerWidth;
-  if (width <= 743) {
-    return 'mobile';
-  } else if (width <= 1199) {
-    return 'tablet';
-  } else {
-    return 'desktop';
+  if (typeof window !== "undefined") {
+    // 클라이언트 측에서만 window 객체를 사용
+    const width = window.innerWidth;
+    if (width <= 743) {
+      return "mobile";
+    } else if (width <= 1199) {
+      return "tablet";
+    } else {
+      return "desktop";
+    }
   }
+  return "desktop";
 };
 
 const useScreenType = () => {
-  const [screenType, setScreenType] = useState(getScreenType());
+  const [screenType, setScreenType] = useState("desktop");
 
   useEffect(() => {
     const handleResize = () => {
       setScreenType(getScreenType());
     };
 
-    window.addEventListener('resize', handleResize);
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
+    // 브라우저에서만 실행되도록 조건부로 처리
+    if (typeof window !== "undefined") {
+      setScreenType(getScreenType()); // 초기 화면 크기 설정
+      window.addEventListener("resize", handleResize);
+
+      return () => {
+        window.removeEventListener("resize", handleResize);
+      };
+    }
   }, []);
 
   return screenType;
 };
 
 export default useScreenType;
-
