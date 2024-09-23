23fbf52d5495b1b36cfdd143f21f81c5d9f23f1e
DESKTOP-EHH0K4D\SnowRang <ajantang@naver.com>
2024-09-23 17:39:59 +0900

    feat : add api function for codeit api & apply user api in HeaderUser
    미션용 codeit api 추가
    HeaderUser에 user api를 사용해 로그인 상태 변환
    
    참고 : refresh token을 쿠키에서 사용할 수 없어(codeit에서 허용 안함) 임시로 로컬에 저장

--- app/components/HeaderUser.js
+++ app/components/HeaderUser.js
@@ -1,9 +1,15 @@
 "use client";
 
+import { useState, useEffect } from "react";
 import Link from "next/link";
 import classNames from "classnames";
 
 import Profile from "@/app/components/Profile";
+//temp
+// import { signUp } from "@/lib/api-codeit-auth";
+// import { setAccessToken } from "@/lib/token-codeit";
+// import { setRecentUserId } from "@/lib/user-info";
+import { getMyInfo } from "@/lib/api-codeit-user";
 import { PROFILE_H40 } from "../constants/Profile";
 
 import style from "./header-user.module.css";
@@ -15,26 +21,64 @@ function BtnLogin() {
     "object-cover",
     style["btn-login"]
   );
+  //temp
+  // const tempSignUp = () => {
+  //   signUp({
+  //     email: "test-codeit16@codeit.com",
+  //     nickname: "codeit16",
+  //     password: "!codeit16",
+  //     passwordConfirmation: "!codeit16",
+  //   }).then((data) => {
+  //     setRecentUserId("test-codeit16@codeit.com");
+  //     setAccessToken(data.accessToken);
+  //   });
+  // };
 
   return (
     <Link href="/login" target="_self">
-      <button className={btnSignInClass} />
+      <button className={btnSignInClass} /* onClick={tempSignUp}  */ />
     </Link>
   );
 }
 
 export function HeaderUser() {
-  const tempIsSignin = false;
+  const [profileImgUrl, setPofileImgUrl] = useState(null);
+  const [nickname, setNickname] = useState("");
+  const [tempIsSignin, setTempIsSignin] = useState(false);
+
+  const userInfoClass = classNames("flex", "flex-row", "items-center");
+  const userNicknameClass = classNames(
+    "text-2lg",
+    "leading-26",
+    "text-gray-600",
+    "ml-0.8rem"
+  );
 
   const handleProfileClick = () => {
     alert("아직 구현되지 않은 기능입니다");
   };
 
   const result = tempIsSignin ? (
-    <Profile type={PROFILE_H40} onClick={handleProfileClick} />
+    <div className={userInfoClass} onClick={handleProfileClick}>
+      <Profile type={PROFILE_H40} profileImgUrl={profileImgUrl} />
+      <p className={userNicknameClass}>{nickname}</p>
+    </div>
   ) : (
     <BtnLogin />
   );
+
+  useEffect(() => {
+    getMyInfo()
+      .then((data) => {
+        setTempIsSignin(true);
+        setPofileImgUrl(data.image);
+        setNickname(data.nickname);
+      })
+      .catch((err) => {
+        console.log(err);
+        setTempIsSignin(false);
+      });
+  }, []);
 
   return result;
 }
