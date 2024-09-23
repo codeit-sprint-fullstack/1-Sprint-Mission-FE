968d746d4a6c97e36995d216c7d53546ab9be4a5
minseokim <claudia503@naver.com>
2024-09-23 17:17:34 +0900

    feat: mission9_SortOptions.js 옵션 정렬 컴포넌트

--- src/components/SortOptions.js
+++ src/components/SortOptions.js
@@ -1,21 +1,45 @@
-import React from "react";
+import { useState } from "react";
 import styles from "./SortOptions.module.css";
 
-const SortOptions = ({ sortOrder, setPage, screenType }) => {
+const SortOptions = ({ sortOrder, setSortOrder, screenType }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
   const handleSortClick = () => {
-    // 모바일에서는 버튼 클릭 시 sort-options를 토글
+    // 모바일에서 버튼 클릭 시 sort-options를 토글
     if (screenType === "mobile") {
-      document.querySelector(`.${styles.sortOptions}`).classList.toggle(styles.active);
+      setIsOpen(!isOpen);
     }
   };
 
   return (
     <div className={styles.sortOptionsContainer}>
       {screenType === "mobile" ? (
-        <div onClick={handleSortClick} className={styles.sortButton} />
+        <div className={styles.sortButton} onClick={handleSortClick} />
       ) : (
         <div className={styles.sortOptions}>
-          <select value={sortOrder} onChange={() => setPage(1)} className={styles.sortSelect}>
+          <select
+            className={styles.sortSelect}
+            value={sortOrder}
+            onChange={(e) => setSortOrder(e.target.value)}
+          >
+            <option value="recent">최신순</option>
+            <option value="popular">인기순</option>
+          </select>
+          <img
+            src="/image/down.svg"
+            alt="Dropdown Icon"
+            className={styles.dropdownIcon}
+          />
+        </div>
+      )}
+
+      {isOpen && screenType === "mobile" && (
+        <div className={`${styles.sortOptions} ${styles.active}`}>
+          <select
+            className={styles.sortSelect}
+            value={sortOrder}
+            onChange={(e) => setSortOrder(e.target.value)}
+          >
             <option value="recent">최신순</option>
             <option value="popular">인기순</option>
           </select>
