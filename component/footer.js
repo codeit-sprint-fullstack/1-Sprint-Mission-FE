dae6ebf323ba2283777386b9271f1e238391bf23
minsu <a2238737@gmail.com>
2024-09-23 10:35:53 +0900

    Feat : Header, footer

---
+++ component/footer.js
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import styles from "./footer.module.css";
+export default function Footer() {
+  return (
+    <>
+      <div className={styles.FooterContainer}>
+        <div className={styles.FooterContent}>
+          <p className={styles.FooterTitle}>@codeit - 2024</p>
+          <div className={styles.FooterMenu}>
+            <p>Privacy Policy</p>
+            <p>FAQ</p>
+          </div>
+          <div className={styles.FooterIcon}>
+            <Image src="/facebook.svg" alt="logo" width={20} height={20} />
+            <Image src="/twitter.svg" alt="logo" width={20} height={20} />
+            <Image src="/youtube.svg" alt="logo" width={20} height={20} />
+            <Image src="/instagram.svg" alt="logo" width={20} height={20} />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
