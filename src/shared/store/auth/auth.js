49681d88ce247af65531420ff92298f5c9613562
mini-1018 <anjaemin1018@gmail.com>
2024-09-29 22:01:46 +0900

    feat : zustand 인증 스토어 추가
    모든 경로의 페이지 로드 시(루트 레이아웃에 적용) 쿠키에 저장된 토큰으로 로그인유지 또는 토큰이 없거나 유효하지 않을 시 재요청, 로그인페이지로 리다이렉트

--- src/shared/store/auth/auth.js
+++ src/shared/store/auth/auth.js
@@ -1,24 +1,90 @@
+import { getUser, postRefreshToken } from '@utils/api/codeit';
 import { create } from 'zustand';
+
+function getCookie(name) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop().split(';').shift();
+  return null;
+}
+
+function setCookie(name, value, days) {
+  const expires = new Date(
+    Date.now() + days * 24 * 60 * 60 * 1000
+  ).toUTCString();
+  document.cookie = `${name}=${value}; expires=${expires}; path=/; secure; samesite=strict;`;
+}
+
+function deleteCookie(name) {
+  document.cookie = `${name}=; Max-Age=0; path=/;`;
+}
 
 export const useAuthStore = create((set) => ({
   user: null,
-  isLoggedIn: false,
 
-  login: ({ user }) =>
+  loadUserFromSession: () => {
+    const user = sessionStorage.getItem('user');
+    if (user) {
+      set({ user: JSON.parse(user) });
+    }
+  },
+
+  login: ({ user }) => {
     set({
       user,
-      isLoggedIn: true,
-    }),
+    });
+    sessionStorage.setItem('user', JSON.stringify(user));
+  },
 
-  logout: () =>
+  logout: () => {
     set({
       user: null,
-      isLoggedIn: false,
-    }),
+    });
+    sessionStorage.removeItem('user');
+    deleteCookie.remove('accessToken');
+    deleteCookie.remove('refreshToken');
+  },
 
   update: ({ user }) =>
     set({
       user,
-      isLoggedIn: false,
     }),
+
+  clearUser: () => {
+    set({ user: null });
+    sessionStorage.removeItem('user');
+  },
+
+  checkAuth: async () => {
+    let accessToken = getCookie('accessToken');
+    const refreshToken = getCookie('refreshToken');
+
+    try {
+      const user = await getUser({
+        Authorization: `Bearer ${accessToken}`,
+      });
+      sessionStorage.setItem('user', JSON.stringify(user));
+      set({ user });
+    } catch (error) {
+      if (error.response?.status === 401 && refreshToken) {
+        try {
+          const newAccessToken = await postRefreshToken(refreshToken);
+          setCookie('accessToken', newAccessToken, 1);
+
+          const user = await getUser({
+            Authorization: `Bearer ${newAccessToken}`,
+          });
+          sessionStorage.setItem('user', JSON.stringify(user));
+          set({ user });
+        } catch (error) {
+          console.error(error);
+          set({ user: null });
+          sessionStorage.removeItem('user');
+          deleteCookie('refreshToken');
+        }
+      } else {
+        window.location.href = '/login';
+      }
+    }
+  },
 }));
