709c06c1428f51eca1fba9569748e3737dae2249
mini-1018 <anjaemin1018@gmail.com>
2024-09-29 21:55:14 +0900

    feat : 인증, 토큰 api 추가

--- src/utils/api/axios.js
+++ src/utils/api/axios.js
@@ -6,6 +6,7 @@ const articleApi = axios.create({
   headers: {
     'Content-Type': 'application/json',
   },
+  withCredentials: false,
 });
 
 const codeitApi = axios.create({
@@ -13,6 +14,73 @@ const codeitApi = axios.create({
   headers: {
     'Content-Type': 'application/json',
   },
+  withCredentials: false,
 });
+
+// function interceptor(apiClient) {
+//   apiClient.interceptors.request.use(
+//     (config) => {
+//       const accessToken = document.cookie
+//         .split('; ')
+//         .find((row) => row.startsWith('accessToken='))
+//         ?.split('=')[1];
+
+//       if (accessToken) {
+//         config.headers['Authorization'] = `Bearer ${accessToken}`;
+//       }
+
+//       return config;
+//     },
+//     (error) => {
+//       return Promise.reject(error);
+//     }
+//   );
+
+//   apiClient.interceptors.response.use(
+//     (response) => {
+//       return response;
+//     },
+//     async (error) => {
+//       const originalRequest = error.config;
+
+//       if (error.response.status === 401 && !originalRequest._retry) {
+//         originalRequest._retry = true;
+
+//         const refreshToken = document.cookie
+//           .split('; ')
+//           .find((row) => row.startsWith('refreshToken='))
+//           ?.split('=')[1];
+
+//         if (refreshToken) {
+//           try {
+//             const response = await axios.post('/auth/refresh-token', {
+//               refreshToken,
+//             });
+
+//             const newAccessToken = response.data?.accessToken;
+
+//             document.cookie = `accessToken=${newAccessToken}; path=/; secure; samesite=strict; expires=${new Date(
+//               new Date().getTime() + 24 * 60 * 60 * 1000
+//             ).toUTCString()}`;
+
+//             originalRequest.headers[
+//               'Authorization'
+//             ] = `Bearer ${newAccessToken}`;
+//             return apiClient(originalRequest);
+//           } catch (err) {
+//             console.error('Token refresh failed:', err);
+//             window.location.href = '/login';
+//           }
+//         } else {
+//           window.location.href = '/login';
+//         }
+//       }
+
+//       return Promise.reject(error);
+//     }
+//   );
+// }
+
+// interceptor(codeitApi);
 
 export { articleApi, codeitApi };
--- src/utils/api/codeit.js
+++ src/utils/api/codeit.js
@@ -8,11 +8,15 @@ export async function postSignIn(params) {
     const refreshToken = response.data?.refreshToken;
 
     if (accessToken) {
-      localStorage.setItem('accessToken', accessToken);
+      document.cookie = `accessToken=${accessToken}; path=/; secure; samesite=strict; expires=${new Date(
+        new Date().getTime() + 24 * 60 * 60 * 1000
+      ).toUTCString()}`;
     }
 
     if (refreshToken) {
-      localStorage.setItem('refreshToken', refreshToken);
+      document.cookie = `refreshToken=${refreshToken}; path=/; secure; samesite=strict; expires=${new Date(
+        new Date().getTime() + 14 * 24 * 60 * 60 * 1000
+      ).toUTCString()}`;
     }
 
     return response.data;
@@ -46,6 +50,29 @@ export async function getProductList(query) {
       params: query,
     });
     return response.data.list;
+  } catch (error) {
+    console.error(error.status);
+  }
+}
+
+export async function getUser(headers) {
+  try {
+    const response = await codeitApi.get(`/users/me`, {
+      headers: headers,
+    });
+    return response.data;
+  } catch (error) {
+    console.error(error.status);
+    throw error;
+  }
+}
+
+export async function postRefreshToken(refreshToken) {
+  try {
+    const response = await codeitApi.post(`/auth/refresh-token`, {
+      refreshToken,
+    });
+    return response.data.accessToken;
   } catch (error) {
     console.error(error.status);
   }
