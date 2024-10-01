c6a3ce0658dbde2b853fce8ac52d464d717ab1d3
S2OHEE <s2h110@naver.com>
2024-10-01 16:32:03 +0900

    BestProductItem에서 images 배열 존재하는지 확인 및 t push  git commit -m 'BestProductItem에서 images 배열 존재여부 확인 및 없는 경우 기본이미지 사용하도록 추가'

--- src/components/BestProductItem.js
+++ src/components/BestProductItem.js
@@ -1,10 +1,16 @@
 import React from "react";
+import styles from "./BestProducts.module.css"; // CSS 모듈 import
 
 function BestProductItem({ product }) {
+  const imageSrc =
+    product.images && product.images.length > 0
+      ? product.images[0]
+      : "/images/default_image.png"; // 기본 이미지 경로
+
   return (
     <div className={styles.bestProductItemCard}>
       <img
-        src={product.images[0]}
+        src={imageSrc}
         alt={product.name}
         className={styles.bestProductImage}
       />
