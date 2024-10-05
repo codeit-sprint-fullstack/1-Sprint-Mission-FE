0eb40bc5d01d71d0a6d27ee0865f4004e4083df1
mjpark <pmj9498@gmail.com>
2024-10-05 17:19:56 +0900

    chore: Header 컴포넌트 수정

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -1,11 +1,33 @@
 import Image from "next/image";
 import basicLogo from "@/images/basicLogo.png";
+import defaultImg from "@/images/defaultUserImg.png";
 import styles from "./Header.module.css";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { getUser } from "@/lib/authApi";
+import { useAuth } from "@/context/authContext";
 
 export default function Header() {
   const router = useRouter();
+  const { user, logout } = useAuth(); // 유저 정보 및 로그아웃 함수 가져오기
+
+  useEffect(() => {
+    // 유저 정보 가져오기
+    const fetchUser = async () => {
+      try {
+        const user = await getUser(); // 유저 정보 API 호출
+      } catch (error) {
+        console.error("유저 정보 가져오기 실패:", error);
+      }
+    };
+    fetchUser();
+  }, []);
+
+  console.log(user);
+  // const { nickname, image } = user;
+  const nickname = user?.nickname;
+  const image = user?.image;
 
   return (
     <header
@@ -39,9 +61,22 @@ export default function Header() {
         </div>
       </div>
       <div className={styles.buttonContainer}>
-        <Link href="/login">
-          <button className={styles.login}>로그인</button>
-        </Link>
+        {/* 임시 사용 로그 아웃 */}
+        {user && (
+          <button className={styles.logout} onClick={logout}>
+            로그아웃
+          </button>
+        )}
+        {user ? (
+          <div className={styles.user}>
+            <Image src={image ? image : defaultImg} alt="user-image" />
+            <span>{nickname}</span>
+          </div>
+        ) : (
+          <Link href="/login">
+            <button className={styles.login}>로그인</button>
+          </Link>
+        )}
       </div>
     </header>
   );
