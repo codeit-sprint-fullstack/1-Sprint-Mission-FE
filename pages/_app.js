3e8075817690eb53182feefacd2e957fa54f2f41
mjpark <pmj9498@gmail.com>
2024-10-05 17:20:49 +0900

    chore: context로 전역 데이터 관리

--- pages/_app.js
+++ pages/_app.js
@@ -2,18 +2,39 @@ import Head from "next/head";
 import { Footer } from "@/components/Layout/Footer";
 import Header from "@/components/Layout/Header";
 import "@/styles/globals.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import React from "react";
+import { AuthProvider } from "@/context/authContext";
 
 export default function App({ Component, pageProps }) {
+  // const queryClient = new QueryClient();
+  const [queryClient] = React.useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 60 * 1000,
+          },
+        },
+      })
+  );
+
   return (
     <>
       <Head>
         <title>판다마켓</title>
       </Head>
-      <div id="page-container">
-        <Header />
-        <Component {...pageProps} />
-        <Footer />
-      </div>
+      <QueryClientProvider client={queryClient}>
+        <div id="page-container">
+          <AuthProvider>
+            <Header />
+            <Component {...pageProps} />
+          </AuthProvider>
+          <Footer />
+          <ReactQueryDevtools initialIsOpen={false} />
+        </div>
+      </QueryClientProvider>
     </>
   );
 }
