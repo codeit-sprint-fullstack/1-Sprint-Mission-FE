13cad9892add91ea1bd6d6f7197dabc6347a08ed
mini-1018 <anjaemin1018@gmail.com>
2024-10-06 21:48:48 +0900

    feat : 상품 관련 리액트 쿼리, 주스탄드 상태관리

--- src/hooks/Constant/queryKeys.js
+++ src/hooks/Constant/queryKeys.js
@@ -1,4 +1,6 @@
 export const QUERY_KEYS = {
   ARTICLE_COMMENTS: 'articleComments',
   SEARCH_ARTICLES: 'searchArticles',
+  PRODUCT_LIST: 'productList',
+  PRODUCT_TOTAL_COUNT: 'productTotalCount',
 };
