0a9cd3dea3e66ef01c973411a6f82782aadda5eb
mini-1018 <anjaemin1018@gmail.com>
2024-11-10 18:07:48 +0900

    feat : auth page

---
+++ src/features/UserAuth/LoginForm.tsx
@@ -0,0 +1,40 @@
+"use client";
+import Button from "@/shared/component/button/button";
+import Input from "@/shared/component/Input/Input";
+import { useState } from "react";
+
+export default function LoginForm() {
+  const [visible, setVisible] = useState<boolean>(false);
+
+  const toggle = () => setVisible(!visible);
+
+  return (
+    <div className="flex flex-col space-y-[24px]">
+      <div className="space-y-[16px]">
+        <div className="text-[18px] font-bold">이메일</div>
+        <Input option="default" placeholder="이메일을 입력해주세요" />
+      </div>
+      <div className="space-y-[16px]">
+        <div className="text-[18px] font-bold">비밀번호</div>
+        <Input
+          option="password"
+          visibility={visible}
+          toggle={toggle}
+          placeholder="비밀번호를 입력해주세요"
+        />
+      </div>
+      <Button
+        buttonText="로그인"
+        styles={{
+          width: "640px",
+          height: "56px",
+          rounded: "40px",
+          background: "#3692ff",
+          color: "white",
+          text: "20px",
+          font: "semibold",
+        }}
+      />
+    </div>
+  );
+}
