0cea3fc6b7e946eae0a82cf358dfcba209a67f3f
juyeongeun <80696592+juyeongeun@users.noreply.github.com>
2024-11-18 15:59:26 +0900

    fix: baseUrl

--- src/utils/apiClient.ts
+++ src/utils/apiClient.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "http://3.35.133.210:3001";
+const baseUrl = "http://yeongeun.click/";
 const apiClient = axios.create({
   baseURL: baseUrl,
   headers: {
