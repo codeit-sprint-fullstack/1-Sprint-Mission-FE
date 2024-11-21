9d4ac3f617aa357f47380e80b006def3f151277e
DESKTOP-EHH0K4D\SnowRang <ajantang@naver.com>
2024-11-21 16:23:18 +0900

    style : replace best post module css to tailwind

--- src/app/bulletin-board/BestPost.tsx
+++ src/app/bulletin-board/BestPost.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import classNames from "classnames";
 
 import Favorite from "../components/Favorite";
 import Date from "../components/Date";
@@ -25,14 +26,69 @@ export function BestPost({
   favoriteCount: number;
   createdDate: string;
 }) {
-  const bestPostClass = `${style["best-post"]}`;
-  const bestPostContentClass = `${style.content}`;
-  const badgeFrameClass = `${style["badge-frame"]}`;
-  const badgeImgClass = `${style["badge-img"]}`;
-  const postMainClass = `flex flex-row justify-between ${style["post-main"]}`;
-  const postNameClass = `font-semibold ${style["post-name"]}`;
-  const postBottomBarClass = `flex flex-row justify-between items-center ${style["post-bottom-bar"]}`;
-  const postBottomNicknameClass = `flex items-center font-normal text-gray-600 align-middle ${style["post-bottom-nickname"]}`;
+  const bestPostClass = classNames(
+    "w-[38.4rem]",
+    "h-[16.9rem]",
+    "bg-alabaster",
+    "box-border",
+    "rounded-[0.8rem]",
+    "ta:w-[34rem]",
+    "ta:h-[19.8rem]",
+    "mo:w-[34.3rem]",
+    "mo:h-[19.8rem]"
+  );
+  const bestPostContentClass = classNames(
+    "w-[33.6rem]",
+    "h-[15.3rem]",
+    "my-0",
+    "mx-auto",
+    "ta:w-[29.2rem]",
+    "ta:h-[18.2rem]",
+    "mo:w-[29.5rem]",
+    "mo:h-[18.2rem]"
+  );
+  const badgeFrameClass = classNames("w-[10rem]", "h-[3rem]", "relative");
+  const postMainClass = classNames(
+    "flex",
+    "flex-row",
+    "justify-between",
+    "w-full",
+    "h-[7.2rem]",
+    "mt-[1.6rem]"
+  );
+  const postNameClass = classNames(
+    "font-semibold",
+    "w-[25.6rem]",
+    "h-full",
+    "text-[2rem]",
+    "leading-[3.2rem]",
+    "ta:w-[18rem]",
+    "mo:w-[18.3rem]",
+    "ta:text-[1.8rem]",
+    "mo:text-[1.8rem]",
+    "ta:leading-[2.6rem]",
+    "mo:leading-[2.6rem]"
+  );
+  const postBottomBarClass = classNames(
+    "flex",
+    "flex-row",
+    "justify-between",
+    "items-center",
+    "w-full",
+    "h-[2.4rem]",
+    "mt-[1.8rem]",
+    "ta:mt-[4rem]",
+    "mo:mt-[4rem]"
+  );
+  const postBottomNicknameClass = classNames(
+    "flex",
+    "items-center",
+    "font-normal",
+    "text-gray-600",
+    "align-middle",
+    "mr-[0.8rem]",
+    "text-[1.4rem]"
+  );
 
   const handleClickPost = () => {};
   // console.log("BestPost postId : ", postId);
@@ -43,7 +99,11 @@ export function BestPost({
       <div className={bestPostClass}>
         <div className={bestPostContentClass}>
           <div className={badgeFrameClass}>
-            <img className={badgeImgClass} alt="베스트 게시글 마크" />
+            <Image
+              src={"/images/img_badge.svg"}
+              alt="베스트 게시글 마크"
+              fill
+            />
           </div>
           <div className={postMainClass}>
             <div className={postNameClass}>{name}</div>
